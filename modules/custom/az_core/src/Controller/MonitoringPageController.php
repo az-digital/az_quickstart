709f4a8c6d1c5e93fbe25c0ab9cac5e11b9c3f08
Joe Parsons <parsonsj@email.arizona.edu>
2019-10-18 17:25:17 -0700

    Implemented page cache policy for monitoring page the preferred way.  Enabled az_core module by default.

--- modules/custom/az_core/src/Controller/MonitoringPageController.php
+++ modules/custom/az_core/src/Controller/MonitoringPageController.php
@@ -3,36 +3,11 @@
 namespace Drupal\az_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class MonitoringPageController.
  */
 class MonitoringPageController extends ControllerBase {
-
-  /**
-   * The page cache kill switch.
-   *
-   * @var \Drupal\Core\PageCache\ResponsePolicy\KillSwitch
-   */
-  protected $pageCacheKillSwitch;
-
-  /**
-   * Constructs a new MonitoringPageController object.
-   */
-  public function __construct(KillSwitch $page_cache_kill_switch) {
-    $this->pageCacheKillSwitch = $page_cache_kill_switch;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('page_cache_kill_switch')
-    );
-  }
 
   /**
    * Deliver.
@@ -41,7 +16,6 @@ class MonitoringPageController extends ControllerBase {
    *   Return monitoring page render array.
    */
   public function deliver() {
-    $this->pageCacheKillSwitch->trigger();
     return [
       '#type' => 'markup',
       '#markup' => '<p>This page is intended for use with uptime monitoring tools.</p>',
