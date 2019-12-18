c561e5e7c270a701852864c702e2d5df581becf5
Troy Dean <tadean@email.arizona.edu>
2019-12-18 13:52:05 -0700

    Add functionality to allow module overrides.

--- modules/custom/az_core/src/QuickstartConfigInstaller.php
+++ modules/custom/az_core/src/QuickstartConfigInstaller.php
@@ -2,13 +2,12 @@
 
 namespace Drupal\az_core;
 
+use Drupal\Component\Serialization\Yaml;
 use Drupal\Core\Config\ConfigInstaller;
-use Drupal\Core\Config\ConfigInstallerInterface;
-use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\FileStorage;
+use Drupal\Core\Config\PreExistingConfigException;
 use Drupal\Core\Config\StorageInterface;
-use Drupal\Core\Config\TypedConfigManagerInterface;
-use Drupal\Core\Config\ConfigManagerInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Drupal\Core\Config\UnmetDependenciesException;
 
 /**
  * Class for customizing the test for pre existing configuration.
@@ -18,22 +17,106 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 class QuickstartConfigInstaller extends ConfigInstaller {
 
   /**
-   * The configuration installer.
-   *
-   * @var \Drupal\Core\Config\ConfigInstallerInterface
-   */
-  protected $configInstaller;
-
-  /**
    * {@inheritdoc}
    */
-  protected function findPreExistingConfiguration(StorageInterface $storage) {
-    // Override
-    // Drupal\Core\Config\ConfigInstaller::findPreExistingConfiguration().
-    // Allow config that already exists.
+  public function checkConfigurationToInstall($type, $name) {
+    if ($this
+      ->isSyncing()) {
 
-    $existing_configuration = [];
-    return $existing_configuration;
+      // Configuration is assumed to already be checked by the config importer
+      // validation events.
+      return;
+    }
+    $config_install_path = $this
+      ->getDefaultConfigDirectory($type, $name);
+    if (!is_dir($config_install_path)) {
+      return;
+    }
+    $storage = new FileStorage($config_install_path, StorageInterface::DEFAULT_COLLECTION);
+    $enabled_extensions = $this
+      ->getEnabledExtensions();
+
+    // Add the extension that will be enabled to the list of enabled extensions.
+    $enabled_extensions[] = $name;
+
+    // Gets profile storages to search for overrides if necessary.
+    $profile_storages = $this
+      ->getProfileStorages($name);
+
+    // Check the dependencies of configuration provided by the module.
+    list($invalid_default_config, $missing_dependencies) = $this
+      ->findDefaultConfigWithUnmetDependencies($storage, $enabled_extensions, $profile_storages);
+    if (!empty($invalid_default_config)) {
+      throw UnmetDependenciesException::create($name, array_unique($missing_dependencies, SORT_REGULAR));
+    }
+
+    // Install profiles can not have config clashes. Configuration that
+    // has the same name as a module's configuration will be used instead.
+    if ($name != $this
+      ->drupalGetProfile()) {
+
+      // Throw an exception if the module being installed contains configuration
+      // that already exists. Additionally, can not continue installing more
+      // modules because those may depend on the current module being installed.
+      $existing_configuration = $this
+        ->findPreExistingConfiguration($storage);
+
+      // Allow a module to explicitly specify its configuration overrides.
+      $existing_configuration = $this
+        ->checkAllowedOverrides($existing_configuration, $type, $name);
+
+      // Any overrides that are not explictly stated are errors.
+      if (!empty($existing_configuration)) {
+        throw PreExistingConfigException::create($name, $existing_configuration);
+      }
+    }
+  }
+
+  /**
+   * Check to allow modules to explicitly specify configuration overrides.
+   *
+   * This allows the QuickstartConfigInstaller to give modules an opportunity
+   * to explicitly indicate they are overwriting configuration that already
+   * exists. This is done by modifying the preExistingConfiguration record
+   * before the array is examined for the potential need to throw a
+   * PreExistingConfigException.
+   *
+   * @param array $configuration
+   *   The configuration collection.
+   * @param string $type
+   *   Extension type, eg. theme, module.
+   * @param string $name
+   *   Name of the extension.
+   *
+   * @return array
+   *   Modified configuration collection.
+   */
+  protected function checkAllowedOverrides(array $configuration, string $type, string $name) {
+
+    $modified_configuration = [];
+    $allowed_overrides = [];
+    $app_root = \Drupal::root();
+    $filename = $app_root . '/' . drupal_get_path($type, $name) . '/' . $name . '.az_config_overrides.yml';
+
+    if (file_exists($filename)) {
+      // If an override file exists, parse it for overrides.
+      $yaml = Yaml::decode(file_get_contents($filename));
+      if (!empty($yaml['overrides'])) {
+        foreach ($yaml['overrides'] as $override) {
+          $allowed_overrides[] = $override;
+        }
+      }
+    }
+
+    foreach ($configuration as $collection => $config_names) {
+      // Strip entries in the collection that are allowed overrides.
+      $config_names = array_diff($config_names, $allowed_overrides);
+      if (!empty($config_names)) {
+        $modified_configuration[$collection] = $config_names;
+      }
+    }
+
+    return $modified_configuration;
   }
 
 }
