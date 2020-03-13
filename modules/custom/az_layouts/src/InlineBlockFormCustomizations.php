13609281e90d2ade1f24771d4395cf40d0d2ef61
Troy Dean <tadean@email.arizona.edu>
2020-03-13 16:39:45 -0700

    Add az_layouts service that customizes layout inline block forms.

---
+++ modules/custom/az_layouts/src/InlineBlockFormCustomizations.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\az_layouts;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\block_content\Entity\BlockContent;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class InlineBlockFormCustomizations.
+ */
+class InlineBlockFormCustomizations {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new InlineBlockFormCustomizations object.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * Invoked during the az_layouts hook_form_alter().
+   */
+  public function alterForm(&$form, FormStateInterface $form_state, $form_id) {
+
+    if (isset($form['settings']['block_form']['#block']) && $form['settings']['block_form']['#block'] instanceof BlockContent) {
+
+      // Get bundle label of the block, for generating the placeholder value.
+      $entity_type = $form['settings']['block_form']['#block']->getEntityType()->getBundleEntityType();
+      $bundle = $form['settings']['block_form']['#block']->bundle();
+      $label = $this->entityTypeManager->getStorage($entity_type)->load($bundle)->label();
+
+      // Autogenerate a label based on the custom block type bundle.
+      $form['settings']['label']['#type'] = 'value';
+      if (empty($form['settings']['label']['#default_value'])) {
+        $form['settings']['label']['#default_value'] = t('Inline @label', ['@label' => $label]);
+      }
+
+      // Hide the label display checkbox.
+      $form['settings']['label_display']['#type'] = 'value';
+      $form['settings']['label_display']['#default_value'] = FALSE;
+
+    }
+  }
+
+}
