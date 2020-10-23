6b025f5231b529a6533cd90d86e97e1606290a94
Chris Green <chrisgreen@email.arizona.edu>
2020-10-23 00:03:21 -0700

    Fix syntax

--- modules/custom/az_paragraphs/az_paragraphs_html/src/Plugin/CKEditorPlugin/Ace.php
+++ modules/custom/az_paragraphs/az_paragraphs_html/src/Plugin/CKEditorPlugin/Ace.php
@@ -66,9 +66,9 @@ class Ace extends CKEditorPluginBase implements CKEditorPluginConfigurableInterf
    */
   public function getConfig(Editor $editor) {
     $settings = $editor->getSettings();
-    
+
     return [
-      'startupMode' => !empty($settings['plugins']['ace']['startup_mode']) ? $settings['plugins']['ace']['startup_mode'] : false
+      'startupMode' => !empty($settings['plugins']['ace']['startup_mode']) ? $settings['plugins']['ace']['startup_mode'] : FALSE
     ];
   }
 
@@ -96,7 +96,7 @@ class Ace extends CKEditorPluginBase implements CKEditorPluginConfigurableInterf
       '#type' => 'checkbox',
       '#title' => $this->t('Start editor in "Source" view.'),
       '#description' => $this->t('Starts editor off in "View Source" mode.'),
-      '#default_value' => !empty($settings['plugins']['ace']['startup_mode']) ? $settings['plugins']['ace']['startup_mode'] : true,
+      '#default_value' => !empty($settings['plugins']['ace']['startup_mode']) ? $settings['plugins']['ace']['startup_mode'] : TRUE,
     );
 
     return $form;
