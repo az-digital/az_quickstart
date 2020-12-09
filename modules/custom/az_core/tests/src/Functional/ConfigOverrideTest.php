35848bdecaf58e6efc0dd34336abc3073ec61d7d
Troy Dean <tadean@email.arizona.edu>
2020-12-09 02:16:39 -0700

    Add unit test and config_sync_test compatibility patch.

---
+++ modules/custom/az_core/tests/src/Functional/ConfigOverrideTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\Tests\az_core\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test to ensure Quickstart configuration overrides work correctly.
+ *
+ * @ingroup az_core
+ *
+ * @group az_core
+ */
+class ConfigOverrideTest extends BrowserTestBase {
+
+  /**
+   * The profile to install as a basis for testing.
+   *
+   * @var string
+   */
+  protected $profile = 'az_quickstart';
+
+  /**
+   * @var bool
+   */
+  protected $strictConfigSchema = FALSE;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'az_cas',
+    'az_core',
+    'cas',
+  ];
+
+  /**
+   * Tests a config override was applied successfully.
+   */
+  public function testConfigOverride() {
+
+    // Check that the cas server hostname has been overridden.
+    $hostname = \Drupal::config('cas.settings')->get('server.hostname');
+    $this->assertEquals('webauth.arizona.edu', $hostname);
+  }
+
+}
