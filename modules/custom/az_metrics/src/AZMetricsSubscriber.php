a01dcea6a34bf21e2cb0a7cf319bba23e2897dd0
Kevin Cooper <kevincooper@arizona.edu>
2021-04-02 16:34:02 -0700

    updates

--- modules/custom/az_metrics/src/AZMetricsSubscriber.php
+++ modules/custom/az_metrics/src/AZMetricsSubscriber.php
@@ -3,6 +3,7 @@
 namespace Drupal\az_metrics\EventSubscriber;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -10,7 +11,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class AZMetricsSubscriber implements EventSubscriberInterface {
 
   public function logDomain(GetResponseEvent $event) {
-    echo $event->getRequest()->getBaseUrl();
+    $baseUrl = $event->getRequest()->getBaseUrl();
+    $event->setResponse(Response::create($baseUrl));
     // if ($event->getRequest()->query->get('redirect-me')) {
     //   $event->setResponse(new RedirectResponse('http://example.com/'));
     // }
