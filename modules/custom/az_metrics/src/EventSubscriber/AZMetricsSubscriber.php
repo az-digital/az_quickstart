a13595a5637bff265864d987bf8d4e4aa499f368
Kevin Cooper <kevincooper@arizona.edu>
2021-04-02 18:24:44 -0700

    add some comments

--- modules/custom/az_metrics/src/EventSubscriber/AZMetricsSubscriber.php
+++ modules/custom/az_metrics/src/EventSubscriber/AZMetricsSubscriber.php
@@ -6,24 +6,31 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Class for logging domains.
+ */
 class AZMetricsSubscriber implements EventSubscriberInterface {
 
-  public function logDomain(GetResponseEvent $event) {
+  /**
+   * The method to store the incoming domain in the database.
+   */
+  protected function logDomain(GetResponseEvent $event) {
     $httpHost = $event->getRequest()->getHttpHost();
     $connection = \Drupal::service('database');
     $connection->merge('az_metrics_domains')
-    ->key('domain', $httpHost)
-    ->fields([
-      'domain' => $httpHost,
-      'last_seen' => \Drupal::time()->getRequestTime(),
-    ])->execute();
+      ->key('domain', $httpHost)
+      ->fields([
+        'domain' => $httpHost,
+        'last_seen' => \Drupal::time()->getRequestTime(),
+      ])->execute();
   }
 
   /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::REQUEST][] = array('logDomain');
+    $events[KernelEvents::REQUEST][] = ['logDomain'];
     return $events;
   }
+
 }
