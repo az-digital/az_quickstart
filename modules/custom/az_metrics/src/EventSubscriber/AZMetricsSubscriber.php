2998c35e75d41d06a1ac742cd9584503d45b7443
Kevin Cooper <kevincooper@arizona.edu>
2021-04-05 11:53:01 -0700

    use dependency injection

--- modules/custom/az_metrics/src/EventSubscriber/AZMetricsSubscriber.php
+++ modules/custom/az_metrics/src/EventSubscriber/AZMetricsSubscriber.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\az_metrics\EventSubscriber;
 
+use Drupal\Core\Database\Connection;
+use Drupal\Component\Datetime\Time;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -12,16 +14,42 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class AZMetricsSubscriber implements EventSubscriberInterface {
 
   /**
+   * Drupal\Core\Database\Connection definition.
+   *
+   * @var Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * Drupal\Component\Datetime\Time definition.
+   *
+   * @var Drupal\Component\Datetime\Time
+   */
+  protected $time;
+
+  /**
+   * Constructs a AZMetricsSubscriber object.
+   *
+   * @param \Drupal\Component\Datetime\Time $time
+   *   time object.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The factory for database connection objects.
+   */
+  public function __construct(Connection $connection, Time $time) {
+    $this->connection = $connection;
+    $this->time = $time;
+  }
+
+  /**
    * The method to store the incoming domain in the database.
    */
   public function logDomain(GetResponseEvent $event) {
     $httpHost = $event->getRequest()->getHttpHost();
-    $connection = \Drupal::service('database');
-    $connection->merge('az_metrics_domains')
+    $this->connection->merge('az_metrics_domains')
       ->key('domain', $httpHost)
       ->fields([
         'domain' => $httpHost,
-        'last_seen' => \Drupal::time()->getRequestTime(),
+        'last_seen' => $this->time->getRequestTime(),
       ])->execute();
   }
 
