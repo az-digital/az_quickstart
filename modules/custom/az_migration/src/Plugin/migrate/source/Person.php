a65b83edd408d8a548adbe37248441bd5db30e23
smitahantzios <shantzios@gmail.com>
2021-04-15 18:56:21 +0530

    Update the files and add migration file for the person content type with basic title only.

---
+++ modules/custom/az_migration/src/Plugin/migrate/source/Person.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Drupal\az_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Drupal 7 Person node source plugin.
+ *
+ * @MigrateSource(
+ *   id = "az_person"
+ * )
+ */
+class Person extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // This queries the built-in metadata, but not the body, tags, or images.
+    $query = $this->select('node', 'n')
+      ->condition('n.type', 'uaqs_person')
+      ->fields('n', [
+        'nid',
+        'vid',
+        'type',
+        'language',
+        'title',
+        'uid',
+        'status',
+        'created',
+        'changed',
+        'promote',
+        'sticky',
+      ]);
+    $query->orderBy('nid');
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = $this->baseFields();
+    $fields['body/format'] = $this->t('Format of body');
+    $fields['body/value'] = $this->t('Full text of body');
+    $fields['body/summary'] = $this->t('Summary of body');
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+
+    // Get the source nid.
+    $nid = $row->getSourceProperty('nid');
+
+    /*
+    Extra field will come here.
+     */
+
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    $ids['nid']['type'] = 'integer';
+    $ids['nid']['alias'] = 'n';
+    return $ids;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function bundleMigrationRequired() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function entityTypeId() {
+    return 'node';
+  }
+
+  /**
+   * Returns the user base fields to be migrated.
+   *
+   * @return array
+   *   Associative array having field name as key and description as value.
+   */
+  protected function baseFields() {
+    $fields = [
+      'nid' => $this->t('Node ID'),
+      'vid' => $this->t('Version ID'),
+      'type' => $this->t('Type'),
+      'title' => $this->t('Title'),
+      'format' => $this->t('Format'),
+      'teaser' => $this->t('Teaser'),
+      'uid' => $this->t('Authored by (uid)'),
+      'created' => $this->t('Created timestamp'),
+      'changed' => $this->t('Modified timestamp'),
+      'status' => $this->t('Published'),
+      'promote' => $this->t('Promoted to front page'),
+      'sticky' => $this->t('Sticky at top of lists'),
+      'language' => $this->t('Language (fr, en, ...)'),
+    ];
+    return $fields;
+  }
+
+}
--- modules/custom/az_migration/src/Plugin/migrate/source/Term.php
+++ modules/custom/az_migration/src/Plugin/migrate/source/Term.php
@@ -1,15 +1,10 @@
 <?php
- 
-/**
- * @file
- * Contains \Drupal\az_migration\Plugin\migrate\source\Term.
- */
- 
+
 namespace Drupal\az_migration\Plugin\migrate\source;
- 
+
 use Drupal\migrate\Row;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
- 
+
 /**
  * Drupal 7 taxonomy terms source from database.
  *
@@ -21,53 +16,53 @@ use Drupal\migrate\Plugin\migrate\source\SqlBase;
  * )
  */
 class Term extends SqlBase {
- 
+
   /**
    * {@inheritdoc}
    */
   public function query() {
     $query = $this->select('taxonomy_term_data', 'td')
-      ->fields('td', array('tid', 'vid', 'name', 'description', 'weight', 'format'))
+      ->fields('td', ['tid', 'vid', 'name', 'description', 'weight', 'format'])
       ->distinct();
-    
+
     $query->leftJoin('taxonomy_vocabulary', 'tv', 'td.vid = tv.vid');
 
-    if(isset($this->configuration['bundle'])){
-        $query->condition('tv.machine_name', (array) $this->configuration['bundle'], 'IN');
+    if (isset($this->configuration['bundle'])) {
+      $query->condition('tv.machine_name', (array) $this->configuration['bundle'], 'IN');
     }
- 
+
     return $query;
   }
- 
+
   /**
    * {@inheritdoc}
    */
   public function fields() {
-    return array(
+    return [
       'tid' => $this->t('The term ID.'),
       'vid' => $this->t('Existing term VID'),
       'name' => $this->t('The name of the term.'),
       'description' => $this->t('The term description.'),
       'weight' => $this->t('Weight'),
       'parent' => $this->t("The Drupal term IDs of the term's parents."),
-    );
+    ];
   }
- 
+
   /**
    * {@inheritdoc}
    */
   public function prepareRow(Row $row) {
-  
+
     // Find parents for this row.
     $parents = $this->select('taxonomy_term_hierarchy', 'th')
-      ->fields('th', array('parent', 'tid'))
+      ->fields('th', ['parent', 'tid'])
       ->condition('tid', $row->getSourceProperty('tid'))
       ->execute()
       ->fetchCol();
     $row->setSourceProperty('parent', $parents);
     return parent::prepareRow($row);
   }
- 
+
   /**
    * {@inheritdoc}
    */
@@ -75,5 +70,5 @@ class Term extends SqlBase {
     $ids['tid']['type'] = 'integer';
     return $ids;
   }
- 
+
 }
