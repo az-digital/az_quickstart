7404c7070432d9dd545683203cb91fb9c8d07a86
smitahantzios <shantzios@gmail.com>
2021-05-21 19:56:54 +0530

    Adding the bottom space to paragraph setting

---
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\az_migration\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Drush\Drush;
+
+/**
+ * Configure Behavior for paragraphs.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "paragraphs_behavior_settings"
+ * )
+ */
+class ParagraphsBehavior extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    // Setting the behavior to the paragraph.
+    $value['behavior'] = serialize(array('az_display_settings' => array( 'bottom_spacing' => $row->getSourceProperty('bottom_spacing'))));
+    return $value['behavior'];
+  }
+
+}
