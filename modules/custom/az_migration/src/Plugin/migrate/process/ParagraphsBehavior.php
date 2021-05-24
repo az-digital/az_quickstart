da59652e011decf4d6466d5aceca34e0319e3fc9
smitahantzios <shantzios@gmail.com>
2021-05-24 10:32:13 +0530

    Adding the bottom space to paragraph setting

---
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\az_migration\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Configure Behavior for paragraphs.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "paragraphs_behavior_settings"
+ * )
+ */
+class ParagraphsBehavior extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    // Setting the behavior to the paragraph.
+    $value['behavior'] = serialize(['az_display_settings' => ['bottom_spacing' => $row->getSourceProperty('bottom_spacing')]]);
+    return $value['behavior'];
+  }
+
+}
---
+++ modules/custom/az_migration/src/Plugin/migrate/source/AZParagraphsItem.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\az_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\paragraphs\Plugin\migrate\source\d7\ParagraphsItem;
+
+/**
+ * Drupal 7 Paragraph Item source plugin.
+ *
+ * @MigrateSource(
+ *   id = "az_paragraphs_item"
+ * )
+ */
+class AZParagraphsItem extends ParagraphsItem {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'item_id' => $this->t('The paragraph_item id'),
+      'revision_id' => $this->t('The paragraph_item revision id'),
+      'bundle' => $this->t('The paragraph bundle'),
+      'field_name' => $this->t('The paragraph field_name'),
+      'bottom_space' => $this->t('The paragraph Bottom Space'),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+
+    // Get the source paragraph item id.
+    $item_id = $row->getSourceProperty('item_id');
+
+    $paragraph_bottom_spacing = $this->select('paragraphs_item', 'pi')
+      ->fields('pi', ['bottom_spacing'])
+      ->condition('pi.item_id', $item_id)
+      ->execute()
+      ->fetchCol();
+    $row->setSourceProperty('bottom_spacing', $paragraph_bottom_spacing);
+    return parent::prepareRow($row);
+  }
+
+}
