bf921e15e3b1116a6491fbf5b4366e4dd114fcc1
smitahantzios <shantzios@gmail.com>
2021-05-26 18:00:56 +0530

    Migration updated - direct mapping the field collection values to the d9 paragraph fields

--- modules/custom/az_migration/src/Plugin/migrate/source/AZParagraphsItem.php
+++ modules/custom/az_migration/src/Plugin/migrate/source/AZParagraphsItem.php
@@ -4,7 +4,6 @@ namespace Drupal\az_migration\Plugin\migrate\source;
 
 use Drupal\migrate\Row;
 use Drupal\paragraphs\Plugin\migrate\source\d7\ParagraphsItem;
-use Drush\Drush;
 
 /**
  * Drupal 7 Paragraph Item source plugin.
@@ -34,9 +33,39 @@ class AZParagraphsItem extends ParagraphsItem {
    */
   public function prepareRow(Row $row) {
 
-    // Get the source paragraph item id.
+    // Get Item Id and revision Id of paragraph.
     $item_id = $row->getSourceProperty('item_id');
-    Drush::output()->writeln($row->getSourceProperty('field_uaqs_carousel'));
+    $revision_id = $row->getSourceProperty('revision_id');
+
+    // Checking the field collection fields present in the paragraph.
+    if (!empty($row->getSourceProperty('field_collection_names'))) {
+      // Getting field collection - fields names from configuration.
+      $field_collection_field_names = explode(',', $row->getSourceProperty('field_collection_names'));
+      foreach ($field_collection_field_names as $field) {
+
+        // Geting field collention values for the paragraph.
+        $field_collection_data = $this->getFieldValues('paragraphs_item', $field, $item_id, $revision_id);
+
+        // Get Field API field values for each field collection item.
+        $field_names = array_keys($this->getFields('field_collection_item', $field));
+
+        foreach ($field_names as $field_collection_field_name) {
+          $i = 0;
+          $field_collection_field_values = [];
+          foreach ($field_collection_data as $field_collection_data_item) {
+            $field_collection_value = $this->getFieldValues('field_collection_item',
+                                      $field_collection_field_name, $field_collection_data_item['value'],
+                                      $field_collection_data_item['revision_id']);
+            $field_collection_value = reset($field_collection_value);
+            $field_collection_value['delta'] = $i;
+            $field_collection_field_values[] = $field_collection_value;
+            $i++;
+          }
+          $row->setSourceProperty($field_collection_field_name, $field_collection_field_values);
+        }
+      }
+    }
+
     $paragraph_bottom_spacing = $this->select('paragraphs_item', 'pi')
       ->fields('pi', ['bottom_spacing'])
       ->condition('pi.item_id', $item_id)
