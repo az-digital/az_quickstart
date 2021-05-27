f22df9adf9f17fe0958fa96bba1ee11c3b91392d
smitahantzios <shantzios@gmail.com>
2021-05-27 16:17:03 +0530

    Update the AZ paragraph source plugin

--- modules/custom/az_migration/src/Plugin/migrate/source/AZParagraphsItem.php
+++ modules/custom/az_migration/src/Plugin/migrate/source/AZParagraphsItem.php
@@ -17,6 +17,28 @@ class AZParagraphsItem extends ParagraphsItem {
   /**
    * {@inheritdoc}
    */
+  public function query() {
+    $query = $this->select('paragraphs_item', 'p')
+      ->fields('p',
+        ['item_id',
+          'bundle',
+          'field_name',
+          'archived',
+          'bottom_spacing',
+        ])
+      ->fields('pr', ['revision_id']);
+    $query->innerJoin('paragraphs_item_revision', 'pr', static::JOIN);
+    // This configuration item may be set by a deriver to restrict the
+    // bundles retrieved.
+    if ($this->configuration['bundle']) {
+      $query->condition('p.bundle', $this->configuration['bundle']);
+    }
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function fields() {
     $fields = [
       'item_id' => $this->t('The paragraph_item id'),
@@ -33,15 +55,38 @@ class AZParagraphsItem extends ParagraphsItem {
    */
   public function prepareRow(Row $row) {
 
-    // Get the source paragraph item id.
+    // Get Item Id and revision Id of paragraph.
     $item_id = $row->getSourceProperty('item_id');
+    $revision_id = $row->getSourceProperty('revision_id');
 
-    $paragraph_bottom_spacing = $this->select('paragraphs_item', 'pi')
-      ->fields('pi', ['bottom_spacing'])
-      ->condition('pi.item_id', $item_id)
-      ->execute()
-      ->fetchCol();
-    $row->setSourceProperty('bottom_spacing', $paragraph_bottom_spacing);
+    // Checking the field collection fields present in the paragraph.
+    if (!empty($row->getSourceProperty('field_collection_names'))) {
+      // Getting field collection - fields names from configuration.
+      $field_collection_field_names = explode(',', $row->getSourceProperty('field_collection_names'));
+      foreach ($field_collection_field_names as $field) {
+
+        // Geting field collention values for the paragraph.
+        $field_collection_data = $this->getFieldValues('paragraphs_item', $field, $item_id, $revision_id);
+
+        // Get Field API field values for each field collection item.
+        $field_names = array_keys($this->getFields('field_collection_item', $field));
+
+        foreach ($field_names as $field_collection_field_name) {
+          $i = 0;
+          $field_collection_field_values = [];
+          foreach ($field_collection_data as $field_collection_data_item) {
+            $field_collection_value = $this->getFieldValues('field_collection_item',
+                                      $field_collection_field_name, $field_collection_data_item['value'],
+                                      $field_collection_data_item['revision_id']);
+            $field_collection_value = reset($field_collection_value);
+            $field_collection_value['delta'] = $i;
+            $field_collection_field_values[] = $field_collection_value;
+            $i++;
+          }
+          $row->setSourceProperty($field_collection_field_name, $field_collection_field_values);
+        }
+      }
+    }
     return parent::prepareRow($row);
   }
 
