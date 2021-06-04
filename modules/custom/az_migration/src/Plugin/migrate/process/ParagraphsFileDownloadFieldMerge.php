167264abe4dac593003591996f5082842a728e2a
smitahantzios <shantzios@gmail.com>
2021-06-04 19:28:58 +0530

    Migration of Paragraph File download update with embeded media

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsFileDownloadFieldMerge.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsFileDownloadFieldMerge.php
@@ -5,6 +5,7 @@ namespace Drupal\az_migration\Plugin\migrate\process;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Drupal\media\Entity\Media;
 
 /**
  * Configure Behavior for paragraphs.
@@ -22,12 +23,26 @@ class ParagraphsFileDownloadFieldMerge extends ProcessPluginBase {
     // Merging the data into one field.
     $field_uaqs_download_name = $row->getSourceProperty('field_uaqs_download_name');
     $field_uaqs_download_description = $row->getSourceProperty('field_uaqs_download_description');
-    $field_uaqs_download_file = $row->getSourceProperty('field_uaqs_download_file');
-    $field_uaqs_download_preview = $row->getSourceProperty('field_uaqs_download_preview');
 
     $value['uaqs_text'] = '<div class="border-thick border-top border-azurite"><div class="border card-body">
       <h3>' . $field_uaqs_download_name[0]['value'] . '</h3>';
-    $value['uaqs_text'] .= '</div></div>';
+
+    // Media embeded for field_uaqs_download_file.
+    if (isset($value[0]) && count($value[0])) {
+      foreach ($value[0] as $mid) {
+        $media = Media::load($mid);
+        $value['uaqs_text'] .= '<drupal-media data-align="center" data-entity-type="media" data-entity-uuid="' . $media->get('uuid')->value . '" data-view-mode="default"></drupal-media>';
+      }
+    }
+
+    // Media embeded for field_uaqs_download_preview.
+    if (isset($value[1]) && count($value[1])) {
+      foreach ($value[1] as $mid) {
+        $media = Media::load($mid);
+        $value['uaqs_text'] .= '<drupal-media data-align="center" data-entity-type="media" data-entity-uuid="' . $media->get('uuid')->value . '" data-view-mode="default"></drupal-media>';
+      }
+    }
+    $value['uaqs_text'] .= $field_uaqs_download_description[0]['value'] . '</div></div>';
     return $value['uaqs_text'];
   }
 
