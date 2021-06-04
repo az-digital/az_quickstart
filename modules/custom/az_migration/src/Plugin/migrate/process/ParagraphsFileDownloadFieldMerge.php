efbcfdedc91842b977e4266493d8836c9dcddeb9
smitahantzios <shantzios@gmail.com>
2021-06-04 23:02:02 +0530

    Updating the paragraph file download - div structure

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsFileDownloadFieldMerge.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsFileDownloadFieldMerge.php
@@ -22,10 +22,7 @@ class ParagraphsFileDownloadFieldMerge extends ProcessPluginBase {
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     // Merging the data into one field.
     $field_uaqs_download_name = $row->getSourceProperty('field_uaqs_download_name');
-    $field_uaqs_download_description = $row->getSourceProperty('field_uaqs_download_description');
-
-    $value['uaqs_text'] = '<div class="border-thick border-top border-azurite"><div class="border card-body">
-      <h3>' . $field_uaqs_download_name[0]['value'] . '</h3>';
+    $value['uaqs_text'] = '<h3>' . $field_uaqs_download_name[0]['value'] . '</h3>';
 
     // Media embeded for field_uaqs_download_file.
     if (isset($value[0]) && count($value[0])) {
@@ -42,7 +39,6 @@ class ParagraphsFileDownloadFieldMerge extends ProcessPluginBase {
         $value['uaqs_text'] .= '<drupal-media data-align="center" data-entity-type="media" data-entity-uuid="' . $media->get('uuid')->value . '" data-view-mode="default"></drupal-media>';
       }
     }
-    $value['uaqs_text'] .= $field_uaqs_download_description[0]['value'] . '</div></div>';
     return $value['uaqs_text'];
   }
 
