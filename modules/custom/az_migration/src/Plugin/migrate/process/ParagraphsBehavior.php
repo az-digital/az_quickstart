b332a3ad2b07b30be8ba6fdf5be3a1a45af54c0a
smitahantzios <shantzios@gmail.com>
2021-06-11 16:36:16 +0530

    Paragraph Migration for Well to AZ Cards

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
@@ -20,7 +20,24 @@ class ParagraphsBehavior extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     // Setting the behavior to the paragraph.
-    $value['behavior'] = serialize(['az_display_settings' => ['bottom_spacing' => $row->getSourceProperty('bottom_spacing')]]);
+    $behavior = ['az_display_settings' => ['bottom_spacing' => $row->getSourceProperty('bottom_spacing')]];
+    if (!empty($this->configuration['gallery_display'])) {
+      $behavior['gallery_display'] = $this->configuration['gallery_display'];
+    }
+    if (!empty($this->configuration['card_width'])) {
+      $behavior['card_width'] = $this->configuration['card_width'];
+    }
+    if (!empty($this->configuration['card_style'])) {
+      $behavior['card_style'] = $this->configuration['card_style'];
+    }
+    if (!empty($this->configuration['card_width_sm'])) {
+      $behavior['az_display_settings']['card_width_sm'] = $this->configuration['card_width_sm'];
+    }
+    if (!empty($this->configuration['card_width_xs'])) {
+      $behavior['az_display_settings']['card_width_xs'] = $this->configuration['card_width_xs'];
+    }
+
+    $value['behavior'] = serialize($behavior);
     return $value['behavior'];
   }
 
