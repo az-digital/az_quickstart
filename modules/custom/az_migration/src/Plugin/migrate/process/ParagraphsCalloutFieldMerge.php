03e2422904bee170644b7f7b9f8f37959a1a2cbe
smitahantzios <shantzios@gmail.com>
2021-06-14 18:24:44 -0400

    Update ParagraphsCalloutFieldMerge.php
    Adding h4 inside div callout

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsCalloutFieldMerge.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsCalloutFieldMerge.php
@@ -50,16 +50,18 @@ class ParagraphsCalloutFieldMerge extends ProcessPluginBase {
 
     // Field value mapping.
     $value['markup'] = '';
-    if (!empty($this->configuration['title'])) {
-      $title_field = $row->getSourceProperty($this->configuration['title']);
-      if (!empty($title_field[0]['value'])) {
-        $value['markup'] .= '<h4 class="' . $text_mapping . '">' . $title_field[0]['value'] . '</h4>';
-      }
-    }
+    
     if (!empty($this->configuration['text'])) {
       $field_uaqs_summary = $row->getSourceProperty($this->configuration['text']);
       if (!empty($field_uaqs_summary[0]['value'])) {
-        $value['markup'] .= '<div class="callout ' . $color_mapping . ' ' . $bg_light . '">' . $field_uaqs_summary[0]['value'] . '</div>';
+        $value['markup'] .= '<div class="callout ' . $color_mapping . ' ' . $bg_light . '">' . $field_uaqs_summary[0]['value'];
+      }
+    }
+    
+    if (!empty($this->configuration['title'])) {
+      $title_field = $row->getSourceProperty($this->configuration['title']);
+      if (!empty($title_field[0]['value'])) {
+        $value['markup'] .= '<h4 class="' . $text_mapping . '">' . $title_field[0]['value'] . '</h4></div>';
       }
     }
     return $value['markup'];
