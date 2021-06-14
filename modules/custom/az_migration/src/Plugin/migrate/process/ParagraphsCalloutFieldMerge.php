c194be01542190edec3b48f47f9b3384f8fc26d9
smitahantzios <shantzios@gmail.com>
2021-06-14 15:46:44 +0530

    Update as per PR comment

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsCalloutFieldMerge.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsCalloutFieldMerge.php
@@ -20,16 +20,14 @@ class ParagraphsCalloutFieldMerge extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     // Merging the data into one field.
-    $title_field = $row->getSourceProperty('title_field');
-    $field_uaqs_summary = $row->getSourceProperty('field_uaqs_summary');
-    $field_uaqs_callout_background = $row->getSourceProperty('field_uaqs_callout_background');
-
     // Backgroud Class.
     $bg_light = '';
-    if (!empty($field_uaqs_callout_background[0]['value'])) {
-      $bg_light = 'bg-light';
+    if (!empty($this->configuration['background'])) {
+      $field_uaqs_callout_background = $row->getSourceProperty($this->configuration['background']);
+      if (!empty($field_uaqs_callout_background[0]['value'])) {
+        $bg_light = 'bg-light';
+      }
     }
-
     // Color and text Mapping.
     $field_uaqs_callout_border_color = $row->getSourceProperty('field_uaqs_callout_border_color');
     $color_mapping = '';
@@ -52,13 +50,18 @@ class ParagraphsCalloutFieldMerge extends ProcessPluginBase {
 
     // Field value mapping.
     $value['uaqs_text'] = '';
-    if (!empty($title_field[0]['value'])) {
-      $value['uaqs_text'] .= '<h4 class="' . $text_mapping . '">' . $title_field[0]['value'] . '</h4>';
+    if (!empty($this->configuration['title'])) {
+      $title_field = $row->getSourceProperty($this->configuration['title']);
+      if (!empty($title_field[0]['value'])) {
+        $value['uaqs_text'] .= '<h4 class="' . $text_mapping . '">' . $title_field[0]['value'] . '</h4>';
+      }
     }
-    if (!empty($field_uaqs_summary[0]['value'])) {
-      $value['uaqs_text'] .= '<div class="callout ' . $color_mapping . ' ' . $bg_light . '">' . $field_uaqs_summary[0]['value'] . '</div>';
+    if (!empty($this->configuration['text'])) {
+      $field_uaqs_summary = $row->getSourceProperty($this->configuration['text']);
+      if (!empty($field_uaqs_summary[0]['value'])) {
+        $value['uaqs_text'] .= '<div class="callout ' . $color_mapping . ' ' . $bg_light . '">' . $field_uaqs_summary[0]['value'] . '</div>';
+      }
     }
-
     return $value['uaqs_text'];
   }
 
