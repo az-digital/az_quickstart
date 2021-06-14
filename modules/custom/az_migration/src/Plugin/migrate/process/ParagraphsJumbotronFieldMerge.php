529dea8a8c6bee2c597a450a32e7d1a8c796a5c8
smitahantzios <shantzios@gmail.com>
2021-06-14 17:08:45 +0530

    Update the value key to markup as per PR suggection

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsJumbotronFieldMerge.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsJumbotronFieldMerge.php
@@ -23,18 +23,18 @@ class ParagraphsJumbotronFieldMerge extends ProcessPluginBase {
     $field_uaqs_short_title = $row->getSourceProperty('field_uaqs_short_title');
     $field_uaqs_summary = $row->getSourceProperty('field_uaqs_summary');
     $field_uaqs_links = $row->getSourceProperty('field_uaqs_links');
-    $value['uaqs_text'] = '<div class="jumbotron">';
+    $value['markup'] = '<div class="jumbotron">';
     if (!empty($field_uaqs_short_title[0]['value'])) {
-      $value['uaqs_text'] .= '<h1 class="display-3 mt-0">' . $field_uaqs_short_title[0]['value'] . '</h1>';
+      $value['markup'] .= '<h1 class="display-3 mt-0">' . $field_uaqs_short_title[0]['value'] . '</h1>';
     }
     if (!empty($field_uaqs_summary[0]['value'])) {
-      $value['uaqs_text'] .= '<div class="lead">' . $field_uaqs_summary[0]['value'] . '</div>';
+      $value['markup'] .= '<div class="lead">' . $field_uaqs_summary[0]['value'] . '</div>';
     }
     if (!empty($field_uaqs_links[0]['url'])) {
-      $value['uaqs_text'] .= '<a href="' . $field_uaqs_links[0]['url'] . '" class="' . $field_uaqs_links[0]['attributes']['class'] . '">' . $field_uaqs_links[0]['title'] . '</a>';
+      $value['markup'] .= '<a href="' . $field_uaqs_links[0]['url'] . '" class="' . $field_uaqs_links[0]['attributes']['class'] . '">' . $field_uaqs_links[0]['title'] . '</a>';
     }
-    $value['uaqs_text'] .= '</div>';
-    return $value['uaqs_text'];
+    $value['markup'] .= '</div>';
+    return $value['markup'];
   }
 
 }
