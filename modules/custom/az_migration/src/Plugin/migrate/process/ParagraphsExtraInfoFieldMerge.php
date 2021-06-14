82f5ad006e8fceeeaa554e9eda934b39c540fbef
smitahantzios <shantzios@gmail.com>
2021-06-14 17:06:44 +0530

    Update the value key to markup as per PR suggection

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsExtraInfoFieldMerge.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsExtraInfoFieldMerge.php
@@ -23,17 +23,17 @@ class ParagraphsExtraInfoFieldMerge extends ProcessPluginBase {
     $field_uaqs_short_title = $row->getSourceProperty('field_uaqs_short_title');
     $field_uaqs_body = $row->getSourceProperty('field_uaqs_body');
     $field_uaqs_link = $row->getSourceProperty('field_uaqs_link');
-    $value['uaqs_text'] = '<div class="border-thick border-top border-azurite">
+    $value['markup'] = '<div class="border-thick border-top border-azurite">
       <div class="border card-body">';
     if (!empty($field_uaqs_link[0]['url'])) {
-      $value['uaqs_text'] .= '<h3>More information</h3>';
-      $value['uaqs_text'] .= '<a href="' . $field_uaqs_link[0]['url'] . '" class="' . $field_uaqs_link[0]['attributes']['class'] . '">' . $field_uaqs_link[0]['title'] . '</a>';
+      $value['markup'] .= '<h3>More information</h3>';
+      $value['markup'] .= '<a href="' . $field_uaqs_link[0]['url'] . '" class="' . $field_uaqs_link[0]['attributes']['class'] . '">' . $field_uaqs_link[0]['title'] . '</a>';
     }
-    $value['uaqs_text'] .= '<h2 class="h3">' . $field_uaqs_short_title[0]['value'] . '</h2>
+    $value['markup'] .= '<h2 class="h3">' . $field_uaqs_short_title[0]['value'] . '</h2>
     ' . $field_uaqs_body[0]['value'] . '
     </div>
     </div>';
-    return $value['uaqs_text'];
+    return $value['markup'];
   }
 
 }
