f12ae7b77c5b8c68bfe1beacc087cec84123fc98
smitahantzios <shantzios@gmail.com>
2021-06-15 17:49:33 +0530

    Paragraph Migration for Card Deck to AZ Card

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
@@ -24,6 +24,45 @@ class ParagraphsBehavior extends ProcessPluginBase {
     if (!empty($this->configuration['gallery_display'])) {
       $behavior['gallery_display'] = $this->configuration['gallery_display'];
     }
+    if (!empty($this->configuration['card_width'])) {
+      $behavior['card_width'] = $this->configuration['card_width'];
+    }
+    if (!empty($this->configuration['card_style'])) {
+      $behavior['card_style'] = $this->configuration['card_style'];
+    }
+    if (!empty($this->configuration['card_width_sm'])) {
+      $behavior['az_display_settings']['card_width_sm'] = $this->configuration['card_width_sm'];
+    }
+    if (!empty($this->configuration['card_width_xs'])) {
+      $behavior['az_display_settings']['card_width_xs'] = $this->configuration['card_width_xs'];
+    }
+    // Getting the card width checking field.
+    if (!empty($this->configuration['card_width_field'])) {
+      $card_width_field = $row->getSourceProperty($this->configuration['card_width_field']);
+      $card_count_field = $row->getSourceProperty($this->configuration['card_count']);
+
+      // If the card width field value is Full Width, adding the conditions.
+      if (!empty($card_width_field[0]['value']) &&
+        $card_width_field[0]['value'] === 'full_width') {
+
+        $card_width = [
+          '1' => 'col-md-12 col-lg-12',
+          '2' => 'col-md-6 col-lg-6',
+          '3' => 'col-md-4 col-lg-4',
+          '4' => 'col-md-3 col-lg-3',
+        ];
+
+        $card_width_sm = [
+          '1' => 'col-sm-12',
+          '2' => 'col-sm-6',
+          '3' => 'col-sm-4',
+          '4' => 'col-sm-3',
+        ];
+        $card_count = count($card_count_field);
+        $behavior['card_width'] = $card_width[$card_count];
+        $behavior['az_display_settings']['card_width_sm'] = $card_width_sm[$card_count];
+      }
+    }
     $value['behavior'] = serialize($behavior);
     return $value['behavior'];
   }
