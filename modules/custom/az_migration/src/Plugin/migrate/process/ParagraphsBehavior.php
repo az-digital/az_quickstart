2b210bdf115a056a21c411153e8745fa8640b68f
smitahantzios <shantzios@gmail.com>
2021-06-23 09:04:39 -0400

    Update modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
    Co-authored-by: Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
@@ -82,7 +82,7 @@ class ParagraphsBehavior extends ProcessPluginBase {
           '1' => 'col-sm-12',
           '2' => 'col-sm-6',
           '3' => 'col-sm-6',
-          '4' => 'col-sm-3',
+          '4' => 'col-sm-6',
         ];
         $card_count = count($card_count_field);
         $behavior['card_width'] = $card_width[$card_count];
