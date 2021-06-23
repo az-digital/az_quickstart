a2906e1d70ab5b9c87dad4aac31bc3950146afc4
smitahantzios <shantzios@gmail.com>
2021-06-23 09:05:51 -0400

    Update modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
    Co-authored-by: Joe Parsons <471936+joeparsons@users.noreply.github.com>

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
@@ -84,8 +84,8 @@ class ParagraphsBehavior extends ProcessPluginBase {
           'uaqs_bg_img_content_center' => 'col-md-8 col-lg-6 col-md-offset-2 col-lg-offset-3',
           'uaqs_bg_img_content_right' => 'col-md-8 col-lg-6 col-md-offset-4 col-lg-offset-6',
         ];
+        $behavior['position'] = $media_mode_mapping[$row->getSourceProperty($this->configuration['media_mode'])];
       }
-      $behavior['position'] = $media_mode_mapping[$row->getSourceProperty($this->configuration['media_mode'])];
     }
 
     if (!empty($this->configuration['full_width'])) {
