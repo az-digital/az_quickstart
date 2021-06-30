8a66284ec8692826719da1afa9b44d76033c512f
smitahantzios <shantzios@gmail.com>
2021-06-30 16:06:42 -0400

    Closes #789: Migration for D7 card deck paragraph type (to cards). (#845)

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphsBehavior.php
@@ -108,6 +108,45 @@ class ParagraphsBehavior extends ProcessPluginBase {
       $behavior['az_display_settings']['card_width_xs'] = $this->configuration['card_width_xs'];
     }
 
+    // Getting the card width checking field.
+    if (!empty($this->configuration['card_width_field'])) {
+      $card_width_field = $row->getSourceProperty($this->configuration['card_width_field']);
+      $card_count_field = $row->getSourceProperty($this->configuration['card_count_field']);
+
+      // If the card width field value is Full Width, adding the conditions.
+      if (!empty($card_width_field[0]['value']) &&
+        $card_width_field[0]['value'] === 'full_width') {
+
+        $card_width = [
+          '1' => 'col-md-12 col-lg-12',
+          '2' => 'col-md-6 col-lg-6',
+          '3' => 'col-md-4 col-lg-4',
+          '4' => 'col-md-3 col-lg-3',
+        ];
+
+        $card_width_sm = [
+          '1' => 'col-sm-12',
+          '2' => 'col-sm-6',
+          '3' => 'col-sm-6',
+          '4' => 'col-sm-6',
+        ];
+        $card_count = count($card_count_field);
+        $behavior['card_width'] = $card_width[$card_count];
+        $behavior['az_display_settings']['card_width_sm'] = $card_width_sm[$card_count];
+      }
+    }
+
+    if (!empty($row->getSourceProperty('view_mode'))) {
+      $card_style_map = [
+        'default' => 'card',
+        'full' => 'card',
+        'token' => 'card',
+        'uaqs_landing_grid' => 'card card-borderless',
+        'uaqs_borderless_card' => 'card card-borderless',
+      ];
+      $behavior['card_style'] = $card_style_map[$row->getSourceProperty('view_mode')];
+    }
+
     $value['behavior'] = serialize($behavior);
     return $value['behavior'];
   }
