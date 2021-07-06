99c260b2ab0704f231f1315e7139a350141e78e5
Chris Green <trackleft2@2860655.no-reply.drupal.org>
2021-07-06 15:57:23 -0700

    update comment syntax

--- modules/custom/az_event/src/Plugin/migrate/process/SmartDateCreateRecurrenceOnField.php
+++ modules/custom/az_event/src/Plugin/migrate/process/SmartDateCreateRecurrenceOnField.php
@@ -28,7 +28,7 @@ class SmartDateCreateRecurrenceOnField extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    /**
+    /*
      * @See https://git.drupalcode.org/project/smart_date/-/blob/3.4.x/modules/smart_date_recur/src/Controller/Instances.php#L351
      * @See https://git.drupalcode.org/project/smart_date/-/blob/3.4.x/modules/smart_date_recur/smart_date_recur.module#L547
      */
