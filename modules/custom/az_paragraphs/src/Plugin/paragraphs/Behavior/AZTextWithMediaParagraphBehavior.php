c5c92a31706980c30d55d24b0196cc09e2959d7e
Chris Green <trackleft2@2860655.no-reply.drupal.org>
2021-08-30 09:27:33 -0700

    Adding img-fluid class to picture

--- modules/custom/az_paragraphs/src/Plugin/paragraphs/Behavior/AZTextWithMediaParagraphBehavior.php
+++ modules/custom/az_paragraphs/src/Plugin/paragraphs/Behavior/AZTextWithMediaParagraphBehavior.php
@@ -298,6 +298,9 @@ class AZTextWithMediaParagraphBehavior extends AZDefaultParagraphsBehavior {
         '#theme' => 'responsive_image_formatter',
         '#responsive_image_style_id' => 'az_full_width_background',
         '#item' => $media->field_media_az_image,
+        '#item_attributes' => [
+          'class' => ['img-fluid'],
+        ],
       ];
       $text_on_bottom = [
         '#type' => 'html_tag',
