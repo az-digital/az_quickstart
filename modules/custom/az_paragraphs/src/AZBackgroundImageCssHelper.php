ac819ff73ef0636f8d099923165169503e85e4da
Chris Green <trackleft2@2860655.no-reply.drupal.org>
2021-10-05 16:26:25 -0700

    Update comment for AZBackgroundImageCssHelper

--- modules/custom/az_paragraphs/src/AZBackgroundImageCssHelper.php
+++ modules/custom/az_paragraphs/src/AZBackgroundImageCssHelper.php
@@ -26,7 +26,6 @@ class AZBackgroundImageCssHelper {
       'bg_image_media_query' => 'all',
       'bg_image_important' => 0,
       'bg_image_z_index' => 'auto',
-      'bg_image_path_format' => 'absolute',
     ];
   }
 
@@ -49,6 +48,10 @@ class AZBackgroundImageCssHelper {
    *      - bg_image_attachment: The attachment property (scroll or fixed)
    *      - bg_image_repeat: The repeat settings
    *      - bg_image_background_size: The background size property if necessary
+   *      - bg_image_gradient: A CSS background gradient
+   *      - bg_image_media_query: Set the media query (all, print or screen)
+   *      - bg_image_important: Add the `!important` property to all background properties.
+   *      - bg_image_z_index: The z-index property if necessary
    *    Default settings will be used for any values not provided.
    * @param string $image_style
    *   Optionally add an image style to the image before applying it to the
@@ -69,11 +72,11 @@ class AZBackgroundImageCssHelper {
     $bg_y = $css_settings['bg_image_y'];
     $attachment = $css_settings['bg_image_attachment'];
     $repeat = $css_settings['bg_image_repeat'];
-    $important_set = $css_settings['bg_image_important'];
-    $important = '';
     $background_size = $css_settings['bg_image_background_size'];
     $background_gradient = !empty($css_settings['bg_image_gradient']) ? $css_settings['bg_image_gradient'] . ',' : '';
     $media_query = isset($css_settings['bg_image_media_query']) ? $css_settings['bg_image_media_query'] : NULL;
+    $important_set = $css_settings['bg_image_important'];
+    $important = '';
     $z_index = $css_settings['bg_image_z_index'];
 
     // Handle the background size property.
