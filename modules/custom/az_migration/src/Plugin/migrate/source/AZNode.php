e6ae178d47473e8de5a2e47679f2fddd0485890a
Chris Green <trackleft2@2860655.no-reply.drupal.org>
2021-10-14 11:10:57 -0700

    phpcs fixes

--- modules/custom/az_migration/src/Plugin/migrate/source/AZNode.php
+++ modules/custom/az_migration/src/Plugin/migrate/source/AZNode.php
@@ -14,27 +14,27 @@ use Drupal\node\Plugin\migrate\source\d7\Node as D7Node;
  */
 class AZNode extends D7Node {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function fields() {
-      return ['alias' => $this->t('Path alias')] + parent::fields();
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return ['alias' => $this->t('Path alias')] + parent::fields();
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function prepareRow(Row $row) {
-        // Include path alias.
-        $nid = $row->getSourceProperty('nid');
-        $query = $this->select('url_alias', 'ua')
-          ->fields('ua', ['alias']);
-        $query->condition('ua.source', 'node/' . $nid);
-        $alias = $query->execute()->fetchField();
-        if (!empty($alias)) {
-          $row->setSourceProperty('alias', '/' . $alias);
-        }
-        return parent::prepareRow($row);
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    // Include path alias.
+    $nid = $row->getSourceProperty('nid');
+    $query = $this->select('url_alias', 'ua')
+      ->fields('ua', ['alias']);
+    $query->condition('ua.source', 'node/' . $nid);
+    $alias = $query->execute()->fetchField();
+    if (!empty($alias)) {
+      $row->setSourceProperty('alias', '/' . $alias);
+    }
+    return parent::prepareRow($row);
   }
 
 }
