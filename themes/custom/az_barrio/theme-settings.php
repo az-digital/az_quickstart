55d27c7a33e1d5055d87020d329025d26d4b6a35
John Moore <jwmoore1@email.arizona.edu>
2021-10-27 10:09:07 -0700

    Closes #1074: Fix footer logo URL bug (#1101)
    Co-authored-by: Joe Parsons <471936+joeparsons@users.noreply.github.com>

--- themes/custom/az_barrio/theme-settings.php
+++ themes/custom/az_barrio/theme-settings.php
@@ -339,12 +339,10 @@ function az_barrio_form_system_theme_settings_alter(&$form, FormStateInterface $
       ],
     ],
   ];
-
   $form['footer_logo']['settings']['footer_logo_link_destination'] = [
-    '#required' => TRUE,
-    '#type' => 'textfield',
-    '#title' => t('Footer logo link destination'),
-    '#description' => t('Where should the footer logo link to. Example: &#x3C;front&#x3E;'),
+    '#type' => 'url',
+    '#title' => t('Footer logo external link destination'),
+    '#description' => t('If blank, the footer logo links to the homepage; otherwise, enter an external site URL. Example: https://www.arizona.edu/'),
     '#default_value' => theme_get_setting('footer_logo_link_destination'),
   ];
   $form['footer_logo']['settings']['footer_logo_alt_text'] = [
