cfb3d41efb1b0ba32253b88441efc735baa2ee7a
Joshua Sosa <styx@email.arizona.edu>
2021-11-17 17:23:27 -0700

    Closes #1139: Adjusts full-width paragraphs based on scrollbar size.

---
+++ modules/custom/az_paragraphs/js/az_paragraphs_full_width.js
@@ -0,0 +1,17 @@
+(function ($, Drupal) {
+  Drupal.behaviors.azParagraphsFullWidth = {
+    attach: function (context, settings) {
+      function _calculateScrollbarWidth() {
+        document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+      }
+      // recalculate on resize
+      window.addEventListener('resize', _calculateScrollbarWidth, false);
+
+      // recalculate on dom load
+      document.addEventListener('DOMContentLoaded', _calculateScrollbarWidth, false);
+
+      // recalculate on load (assets loaded as well)
+      window.addEventListener('load', _calculateScrollbarWidth);
+    }
+  };
+})(jQuery, Drupal);
