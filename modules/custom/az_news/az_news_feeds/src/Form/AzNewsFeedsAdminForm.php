e72453f31ab06e92d9a7491cdb77b73213551b5d
Chris Green <chrisgreen@arizona.edu>
2021-11-30 13:24:00 -0700

    Forgot to push this on friday

--- modules/custom/az_news/az_news_feeds/src/Form/AzNewsFeedsAdminForm.php
+++ modules/custom/az_news/az_news_feeds/src/Form/AzNewsFeedsAdminForm.php
@@ -2,16 +2,39 @@
 
 namespace Drupal\az_news_feeds\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\migrate\MigrateExecutable;
+use Drupal\migrate\MigrateMessage;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- *
+ * Form for editing news importer source.
  */
 class AzNewsFeedsAdminForm extends ConfigFormBase {
 
   /**
+   * Constructs a AzNewsFeedsAdminForm object.
    *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    parent::__construct($config_factory);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
     return [
@@ -45,11 +68,31 @@ class AzNewsFeedsAdminForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
 
     $this->config('migrate_plus.migration_group.az_news_feeds')
       ->set('shared_configuration.source.urls', $form_state->getValue('urls'))
       ->save();
+
+      drupal_flush_all_caches();
+
+      $tag = 'Quickstart News Feeds';
+
+      // Rollback the migrations for the old endpoint.
+      $migrations = \Drupal::service('plugin.manager.migration')->createInstancesByTag($tag);
+      foreach ($migrations as $migration) {
+        $executable = new MigrateExecutable($migration, new MigrateMessage());
+        $executable->rollback();
+      }
+
+      // Run the migrations for the new endpoint.
+      $migrations = \Drupal::service('plugin.manager.migration')->createInstancesByTag($tag);
+      foreach ($migrations as $migration) {
+        $executable = new MigrateExecutable($migration, new MigrateMessage());
+        $executable->import();
+      }
+      
+      parent::submitForm($form, $form_state);
+
   }
 
 }
