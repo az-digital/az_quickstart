8c0356297477c2a9e1c437b1c331e759db232f6b
Chris Green <chrisgreen@arizona.edu>
2021-12-15 12:00:18 -0700

    Change selection text to media aspect ratio
    Co-authored-by: Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>

--- modules/custom/az_paragraphs/src/Plugin/paragraphs/Behavior/AZTextWithMediaParagraphBehavior.php
+++ modules/custom/az_paragraphs/src/Plugin/paragraphs/Behavior/AZTextWithMediaParagraphBehavior.php
@@ -139,7 +139,7 @@ class AZTextWithMediaParagraphBehavior extends AZDefaultParagraphsBehavior {
         'y-10' => $this->t('10 (8.0rem | ~128px)'),
         'y-20' => $this->t('20 (16.0rem | ~256px)'),
         'y-30' => $this->t('30 (24.0rem | ~384px)'),
-        'az-aspect-ratio' => $this->t('Background Aspect Ratio'),
+        'az-aspect-ratio' => $this->t('Media Aspect Ratio'),
       ],
       '#default_value' => $config['text_media_spacing'],
       '#description' => $this->t('Adds spacing above and below the text.'),
