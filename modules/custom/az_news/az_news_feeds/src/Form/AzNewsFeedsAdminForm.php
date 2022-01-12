9bc3a1c1bc601fc19faadba64a499bf9be86de92
Chris Green <chrisgreen@arizona.edu>
2022-01-12 11:22:17 -0700

    adding dynamic options for terms

--- modules/custom/az_news/az_news_feeds/src/Form/AzNewsFeedsAdminForm.php
+++ modules/custom/az_news/az_news_feeds/src/Form/AzNewsFeedsAdminForm.php
@@ -8,7 +8,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\migrate\MigrateExecutable;
 use Drupal\migrate\MigrateMessage;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
+use GuzzleHttp\Client;
 /**
  * Form for editing news importer source.
  */
@@ -39,6 +39,7 @@ class AzNewsFeedsAdminForm extends ConfigFormBase {
   protected function getEditableConfigNames() {
     return [
       'migrate_plus.migration_group.az_news_feeds',
+      'az_news_feeds.settings',
     ];
   }
 
@@ -50,15 +51,52 @@ class AzNewsFeedsAdminForm extends ConfigFormBase {
   }
 
   /**
+   * Returns array of term options from UArizona News.
+   */
+  public function getRemoteTermOptions(): array {
+    $az_news_feeds_config = $this->config('az_news_feeds.settings');
+    $base_uri = $az_news_feeds_config->get('uarizona_news_category_base_uri');
+    $category_path = $az_news_feeds_config->get('uarizona_news_category_path');
+
+    $client = new Client([
+      'base_uri' => $base_uri,
+    ]);
+
+    $response = $client->get($category_path, []);
+
+    $terms = json_decode($response->getBody(), TRUE);
+
+    $options = ['all' => 'All'];
+    foreach ($terms['terms']  as $key => $value) {
+      $options[$value['term']['tid']] = $value['term']['name'];
+    }
+
+    return $options;
+  }
+
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $az_news_feeds_config = $this->config('az_news_feeds.settings');
     $config = $this->config('migrate_plus.migration_group.az_news_feeds');
-    $form['urls'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('UArizona News Feed URLs'),
-      '#description' => $this->t('URLs to fetch.'),
-      '#default_value' => $config->get('shared_configuration.source.urls'),
+    $selected_cat = $az_news_feeds_config->get('uarizona_news_terms');
+    $term_options = $this->getRemoteTermOptions();
+
+    $form['term_options'] = [
+      '#type' => 'value',
+      '#value' => $term_options,
+    ];
+
+    $form['uarizona_news_terms'] = [
+      '#title' => t('News Categories'),
+      '#type' => 'select',
+      '#multiple' => TRUE,
+      '#required' => TRUE,
+      '#description' => 'Select which terms you want to use.',
+      '#options' => $form['term_options']['#value'],
+      '#default_value' => $selected_cat,
     ];
 
     return parent::buildForm($form, $form_state);
@@ -68,9 +106,15 @@ class AzNewsFeedsAdminForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $az_news_feeds_config = $this->configFactory->getEditable('az_news_feeds.settings');
+    $terms = implode('+', array_keys($form_state->getValue('uarizona_news_terms')));
+    $az_news_feeds_config
+      ->set('uarizona_news_terms', $form_state->getValue('uarizona_news_terms'))
+      ->save();
 
+    $urls = 'https://news.arizona.edu/feed/json/stories/id/' . $terms;
     $this->config('migrate_plus.migration_group.az_news_feeds')
-      ->set('shared_configuration.source.urls', $form_state->getValue('urls'))
+      ->set('shared_configuration.source.urls', $urls)
       ->save();
 
       drupal_flush_all_caches();
