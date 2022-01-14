9976752c5757f25ddd2ba59ac37d24889679a6e7
Chris Green <chrisgreen@arizona.edu>
2022-01-14 14:01:26 -0700

    Update event subscriber

--- modules/custom/az_news/az_news_feeds/src/EventSubscriber/AzNewsFeedsMigrateSubscriber.php
+++ modules/custom/az_news/az_news_feeds/src/EventSubscriber/AzNewsFeedsMigrateSubscriber.php
@@ -71,36 +71,82 @@ class AzNewsFeedsMigrateSubscriber implements EventSubscriberInterface {
 
     $migration = $event->getMigration();
     if ($migration->id() === 'az_news_feed_stories') {
+      // Change the news.arizona.edu feed url.
       $az_news_feeds_config = $this->configFactory->getEditable('az_news_feeds.settings');
       $selected_terms = $az_news_feeds_config->get('uarizona_news_terms');
       $views_contextual_argument = implode('+', array_keys($selected_terms));
       $urls = 'https://news.arizona.edu/feed/json/stories/id/' . $views_contextual_argument;
       $event_migration = Migration::load($migration->id());
       $source = $event_migration->get('source');
+      $source_count = $migration->getProcess();
       $source['urls'] = $urls;
+      \Drupal::logger('az_news_feeds')->notice(print_r($source_count, TRUE));
+      // $process = [
+      //   'plugin' => 'sub_process',
+      //   'source' => 'field_az_news_tags',
+      //   'process' => [
+      //     'target_id' => [
+      //       [
+      //         'plugin' => 'paragraphs_lookup',
+      //         'tags' => 'Field Collection Content',
+      //         'source' => 'value',
+      //       ],
+      //       [
+      //         'plugin' => 'extract',
+      //         'index' => [
+      //           'id',
+      //         ],
+      //       ],
+      //     ],
+      //     'target_revision_id' => [
+      //       [
+      //         'plugin' => 'paragraphs_lookup',
+      //         'tags' => [
+      //           'Field Collection Revisions Content',
+      //           'Field Collection Content',
+      //         ],
+      //         'tag_ids' => [
+      //           'Field Collection Revisions Content' => [
+      //             'revision_id',
+      //           ],
+      //           'Field Collection Content' => [
+      //             'value',
+      //           ],
+      //         ],
+      //       ],
+      //       [
+      //         'plugin' => 'extract',
+      //         'index' => [
+      //           'revision_id',
+      //         ],
+      //       ],
+      //     ],
+      //   ],
+      // ];
+  
       $event_migration->set('source', $source);
       $event_migration->save();
     }
   }
 
- /**
-  * Event which fires before the Row is going to be saved.
-  *
-  * @param \Drupal\migrate\Event\MigratePreRowSaveEvent $event
-  *  Current MigratePreRowSaveEvent object.
-  */
-  public function onPreRowSave(MigratePreRowSaveEvent $event) {
-    $migration = $event->getMigration();
-    if ($migration->id() === 'az_news_feed_stories') {
-      $sourceTags =  explode(', ', $event->getRow()->getSourceProperty('tags'));
-      $az_news_feeds_config = $this->configFactory->getEditable('az_news_feeds.settings');
-      $selected_terms = array_values($az_news_feeds_config->get('uarizona_news_terms'));
-      $pruned_tags = implode(', ', array_values(array_intersect($sourceTags, $selected_terms)));
-      $row = $event->getRow();
-      $row->setDestinationProperty('field_az_news_tags', $pruned_tags);
-      // \Drupal::logger('az_news_feeds')->notice(print_r($pruned_tags, TRUE));
-    }
-  }
+//  /**
+//   * Event which fires before the Row is going to be saved.
+//   *
+//   * @param \Drupal\migrate\Event\MigratePreRowSaveEvent $event
+//   *  Current MigratePreRowSaveEvent object.
+//   */
+//   public function onPreRowSave(MigratePreRowSaveEvent $event) {
+//     $migration = $event->getMigration();
+//     if ($migration->id() === 'az_news_feed_stories') {
+//       $az_news_feeds_config = $this->configFactory->getEditable('az_news_feeds.settings');
+//       $selected_terms = array_values($az_news_feeds_config->get('uarizona_news_terms'));
+//       $row = $event->getRow();
+//       $sourceTags =  explode(', ', $row->getSourceProperty('tags'));
+//       $pruned_tags = implode(', ', array_values(array_intersect($sourceTags, $selected_terms)));
+//       $row->setDestinationProperty('field_az_news_tags', $pruned_tags);
+//       \Drupal::logger('az_news_feeds')->notice(print_r($pruned_tags, TRUE));
+//     }
+//   }
 
   /**
    * {@inheritdoc}
@@ -108,7 +154,7 @@ class AzNewsFeedsMigrateSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     $events = [];
     $events[MigrateEvents::PRE_IMPORT] = ['onPreImport'];
-    $events[MigrateEvents::PRE_ROW_SAVE] = ['onPreRowSave'];
+    // $events[MigrateEvents::PRE_ROW_SAVE] = ['onPreRowSave'];
 
     return $events;
   }
