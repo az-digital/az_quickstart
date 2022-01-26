43e9747baadfbed6bc4c5472a7d6cfb6530f4f5c
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2022-01-26 10:57:32 -0700

    Update modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php

--- modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php
+++ modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php
@@ -140,7 +140,7 @@ class AZCardDefaultFormatter extends FormatterBase implements ContainerFactoryPl
           if (!empty($parent_config['az_cards_paragraph_behavior'])) {
             $card_defaults = $parent_config['az_cards_paragraph_behavior'];
             // Is the card clickable?
-            if (isset($card_defaults['card_clickable']) && $card_defaults['card_clickable'] != FALSE) {
+            if (isset($card_defaults['card_clickable']) && $card_defaults['card_clickable']) {
               $link_render_array['#attributes']['class'][] = 'stretched-link';
             }
 
