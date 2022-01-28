59b4d7e49339a14bfde65556b254cb7d8514daa5
Chris Green <chrisgreen@arizona.edu>
2022-01-28 10:13:08 -0700

    adding url

--- modules/custom/az_select_menu/src/Plugin/Block/AzSelectMenu.php
+++ modules/custom/az_select_menu/src/Plugin/Block/AzSelectMenu.php
@@ -2,16 +2,9 @@
 
 namespace Drupal\az_select_menu\Plugin\Block;
 
-use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Link;
-use Drupal\Core\Menu\MenuTreeParameters;
-use Drupal\Core\Render\Markup;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Template\Attribute;
 use Drupal\menu_block\Plugin\Block\MenuBlock;
-use Drupal\system\Entity\Menu;
-use Drupal\system\Plugin\Block\SystemMenuBlock;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -48,51 +41,51 @@ class AzSelectMenu extends MenuBlock {
 
     $form = parent::blockForm($form, $form_state);
     $form['az_select_menu'] = [
-        '#type' => 'details',
-        '#title' => $this->t('Quickstart select menu options.'),
+      '#type' => 'details',
+      '#title' => $this->t('Quickstart select menu options.'),
     ];
 
     $form['az_select_menu']['empty_option'] = [
-        '#type' => 'checkbox',
-        '#title' => t('Include an empty option.'),
-        '#default_value' => $config['az_select_menu']['empty_option'],
+      '#type' => 'checkbox',
+      '#title' => t('Include an empty option.'),
+      '#default_value' => $config['az_select_menu']['empty_option'],
     ];
 
     $form['az_select_menu']['empty_option_label'] = [
-        '#type' => 'textfield',
-        '#title' => t('Empty option label.'),
-        '#default_value' => $config['az_select_menu']['empty_option_label'],
-        '#states' => [
-            'visible' => [
-                ':input[name="az_select_menu[empty_option]"]' => ['checked' => TRUE],
-            ],
+      '#type' => 'textfield',
+      '#title' => t('Empty option label.'),
+      '#default_value' => $config['az_select_menu']['empty_option_label'],
+      '#states' => [
+        'visible' => [
+          ':input[name="az_select_menu[empty_option]"]' => ['checked' => TRUE],
         ],
+      ],
     ];
 
     $form['az_select_menu']['preform_text'] = [
-        '#type' => 'textfield',
-        '#title' => t('Text to display inline before the select form.'),
-        '#default_value' => $config['az_select_menu']['preform_text'],
-        '#description' => t('You may use hyphens, underscores, and alphanumeric characters.'),
+      '#type' => 'textfield',
+      '#title' => t('Text to display inline before the select form.'),
+      '#default_value' => $config['az_select_menu']['preform_text'],
+      '#description' => t('You may use hyphens, underscores, and alphanumeric characters.'),
     ];
 
     $form['az_select_menu']['preform_text_sr_only'] = [
-        '#type' => 'textfield',
-        '#title' => t('Form help for screen-readers.'),
-        '#default_value' => $config['az_select_menu']['preform_text_sr_only'],
-        '#description' => t('Depending on the preform text, screen readers don\'t necessarily show their users this form in a helpful context.'),
+      '#type' => 'textfield',
+      '#title' => t('Form help for screen-readers.'),
+      '#default_value' => $config['az_select_menu']['preform_text_sr_only'],
+      '#description' => t('Depending on the preform text, screen readers don\'t necessarily show their users this form in a helpful context.'),
     ];
 
     $form['az_select_menu']['button_text'] = [
-        '#type' => 'textfield',
-        '#title' => t('Text you would like to appear in the button.'),
-        '#default_value' => $config['az_select_menu']['button_text'],
+      '#type' => 'textfield',
+      '#title' => t('Text you would like to appear in the button.'),
+      '#default_value' => $config['az_select_menu']['button_text'],
     ];
 
     $form['az_select_menu']['button_text_sr_only'] = [
-        '#type' => 'textfield',
-        '#title' => t('Text to help screen-reader users understand what to do.'),
-        '#default_value' => $config['az_select_menu']['button_text_sr_only'],
+      '#type' => 'textfield',
+      '#title' => t('Text to help screen-reader users understand what to do.'),
+      '#default_value' => $config['az_select_menu']['button_text_sr_only'],
     ];
 
     return $form;
@@ -104,12 +97,12 @@ class AzSelectMenu extends MenuBlock {
   public function defaultConfiguration() {
     $config = parent::defaultConfiguration();
     $config['az_select_menu'] = [
-        'empty_option' => TRUE,
-        'empty_option_label' => 'choose an option',
-        'preform_text' => 'I am a',
-        'preform_text_sr_only' => 'Select your audience',
-        'button_text' => 'Go',
-        'button_text_sr_only' => ' to the page for that group',
+      'empty_option' => TRUE,
+      'empty_option_label' => 'choose an option',
+      'preform_text' => 'I am a',
+      'preform_text_sr_only' => 'Select your audience',
+      'button_text' => 'Go',
+      'button_text_sr_only' => ' to the page for that group',
     ];
 
     return $config;
@@ -124,14 +117,12 @@ class AzSelectMenu extends MenuBlock {
     $this->configuration['az_select_menu'] = $form_state->getValue('az_select_menu');
   }
 
-
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = parent::build();
     // dpm($build);
-
     $form_attributes = new Attribute([
       'id' => 'az-select-menu-form-' . $build['#menu_name'],
       'class' => [
@@ -178,39 +169,30 @@ class AzSelectMenu extends MenuBlock {
   //  */
   // public function preprocess(&$variables) {
   //   dpm($variables);
-    // // Libraries to attach to this paragraph.
-    // $libraries = [];
-
-    // /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */
-    // $paragraph = $variables['paragraph'];
-
-    // // Get plugin configuration.
-    // $config = $this->getSettings($paragraph);
-
-    // // Get the paragraph bundle name and compute name of potential library.
-    // $bundle = $paragraph->bundle();
-    // $libraries[] = 'az_paragraphs.' . $bundle;
-
-    // // Generate library names to check based on view mode.
-    // if (!empty($variables['view_mode'])) {
-    //   // Potential library for paragraph view mode.
-    //   $libraries[] = 'az_paragraphs.' . $variables['view_mode'];
-    //   // Bundle-specific view mode libraries.
-    //   $libraries[] = 'az_paragraphs.' . $bundle . '_' . $variables['view_mode'];
-    // }
-
-    // // Check if any of the potential library names actually exist.
-    // foreach ($libraries as $name) {
-    //   // Check if library discovery service knows about the library.
-    //   $library = $this->libraryDiscovery->getLibraryByName('az_paragraphs', $name);
-
-    //   if ($library) {
-    //     // If we found a library, attach it to the paragraph.
-    //     $variables['#attached']['library'][] = 'az_paragraphs/' . $name;
-    //   }
-    // }
+  // // Libraries to attach to this paragraph.
+  // $libraries = [];
+  // /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */
+  // $paragraph = $variables['paragraph'];
+  // // Get plugin configuration.
+  // $config = $this->getSettings($paragraph);
+  // // Get the paragraph bundle name and compute name of potential library.
+  // $bundle = $paragraph->bundle();
+  // $libraries[] = 'az_paragraphs.' . $bundle;
+  // // Generate library names to check based on view mode.
+  // if (!empty($variables['view_mode'])) {
+  //   // Potential library for paragraph view mode.
+  //   $libraries[] = 'az_paragraphs.' . $variables['view_mode'];
+  //   // Bundle-specific view mode libraries.
+  //   $libraries[] = 'az_paragraphs.' . $bundle . '_' . $variables['view_mode'];
   // }
-
-
+  // // Check if any of the potential library names actually exist.
+  // foreach ($libraries as $name) {
+  //   // Check if library discovery service knows about the library.
+  //   $library = $this->libraryDiscovery->getLibraryByName('az_paragraphs', $name);
+  // if ($library) {
+  //     // If we found a library, attach it to the paragraph.
+  //     $variables['#attached']['library'][] = 'az_paragraphs/' . $name;
+  //   }
+  // }
+  // }.
 }
-
