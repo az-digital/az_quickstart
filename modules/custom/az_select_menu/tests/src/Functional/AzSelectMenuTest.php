210b5c6cfa5ca5bc094d996aaee63b70c9ee849d
Chris Green <chrisgreen@arizona.edu>
2022-02-04 13:58:08 -0700

    Add some functional tests and change the output for better debugging.

---
+++ modules/custom/az_select_menu/tests/src/Functional/AzSelectMenuTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\Tests\az_select_menu\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the Quickstart select menu block.
+ *
+ * @group az_select_menu
+ */
+class AzSelectMenuTest extends BrowserTestBase {
+
+  /**
+   * The profile to install as a basis for testing.
+   *
+   * @var string
+   */
+  protected $profile = 'az_quickstart';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'az_barrio';
+
+  /**
+   * Disable strict schema cheking.
+   *
+   * @var bool
+   */
+  protected $strictConfigSchema = FALSE;
+
+  /**
+   * The created user.
+   *
+   * @var User
+   */
+  protected $adminUser;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+
+  public static $modules = ['az_select_menu'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create a test user.
+    $this->adminUser = $this->drupalCreateUser([
+      'access administration pages',
+      'administer modules',
+    ]);
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests that the Quickstart Select Menu Block module can be uninstalled.
+   * @group az_select_menu
+   * @testdox The az_select_menu module is uninstallable.
+   */
+  public function testIsUninstallable() {
+
+    // Uninstalls the az_select_menu module, so hook_modules_uninstalled()
+    // is executed.
+    $this->container
+      ->get('module_installer')
+      ->uninstall([
+      'az_select_menu',
+    ]);
+
+  }
+
+}
