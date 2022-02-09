7ced16f93ee51627f31a184ced0a75b93237bc2b
Chris Green <chrisgreen@arizona.edu>
2022-02-09 13:24:27 -0700

    Update themes/custom/az_barrio/theme-settings.php
    Co-authored-by: Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>

--- themes/custom/az_barrio/theme-settings.php
+++ themes/custom/az_barrio/theme-settings.php
@@ -264,7 +264,7 @@ function az_barrio_form_system_theme_settings_alter(&$form, FormStateInterface $
   $form['layout']['region_containers']['header_two_col_classes'] = [
     '#type' => 'textfield',
     '#title' => t('Column two classes'),
-    '#description' => t('Responsive column classes for the parent <code>div</code> of the header 1 and header 2 regions. Should contain a string with classes separated by a space.'),
+    '#description' => t('Responsive column classes for the parent <code>div</code> of the Header 1 and Header 2 regions. Should contain a string with classes separated by a space.'),
     '#default_value' => theme_get_setting('header_two_col_classes'),
     '#element_validate' => ['token_element_validate'],
   ];
