719c9b6d56aa1d3cd5f19251f7fd2e93d0101bf9
Chris Green <chrisgreen@arizona.edu>
2022-02-09 13:24:20 -0700

    Update themes/custom/az_barrio/theme-settings.php

--- themes/custom/az_barrio/theme-settings.php
+++ themes/custom/az_barrio/theme-settings.php
@@ -260,7 +260,6 @@ function az_barrio_form_system_theme_settings_alter(&$form, FormStateInterface $
     '#title' => t('Column one classes'),
     '#description' => t('Responsive column classes for the parent <code>div</code> of the Site branding region. Should contain a string with classes separated by a space.'),
     '#default_value' => theme_get_setting('header_one_col_classes'),
-    '#element_validate' => ['token_element_validate'],
   ];
   $form['layout']['region_containers']['header_two_col_classes'] = [
     '#type' => 'textfield',
