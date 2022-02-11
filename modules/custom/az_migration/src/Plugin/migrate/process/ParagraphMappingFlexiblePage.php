1a2dc83dece5441c8e411adbc44eddfce949f7ac
Lee Marsh <rmarsh1@arizona.edu>
2022-02-11 11:05:25 -0700

    Fixing code standard issues

--- modules/custom/az_migration/src/Plugin/migrate/process/ParagraphMappingFlexiblePage.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/ParagraphMappingFlexiblePage.php
@@ -13,25 +13,24 @@ use Drupal\migrate\Row;
  *   id = "paragraphs_mapping_flexible_page"
  * )
  */
-class ParagraphMappingFlexiblePage extends ProcessPluginBase
-{
+class ParagraphMappingFlexiblePage extends ProcessPluginBase {
 
   /**
    * {@inheritdoc}
    */
-    public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property)
-    {
-        // Merging the data into paragraph field on flexible page.
-        $main_content = [];
-        foreach ($value as $item) {
-            if (isset($item[0]) && isset($item[1])) {
-                $main_content[] = [
-                  'target_id' => $item[0],
-                  'target_revision_id' => $item[1],
-                ];
-            }
-        }
-
-        return $main_content;
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    // Merging the data into paragraph field on flexible page.
+    $main_content = [];
+    foreach ($value as $item) {
+      if (isset($item[0]) && isset($item[1])) {
+        $main_content[] = [
+          'target_id' => $item[0],
+          'target_revision_id' => $item[1],
+        ];
+      }
     }
+
+    return $main_content;
+  }
+
 }
