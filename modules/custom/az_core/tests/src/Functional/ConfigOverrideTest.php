8306ba23bc6030dd35e04733cfe744db814604fa
Mark Fischer <fischerm@email.arizona.edu>
2022-04-01 10:22:45 -0700

    Closes #1426: Change default CAS hostname to shibboleth.arizona.edu (#1431)

--- modules/custom/az_core/tests/src/Functional/ConfigOverrideTest.php
+++ modules/custom/az_core/tests/src/Functional/ConfigOverrideTest.php
@@ -48,7 +48,7 @@ class ConfigOverrideTest extends BrowserTestBase {
 
     // Check that the cas server hostname has been overridden.
     $hostname = $this->config('cas.settings')->get('server.hostname');
-    $this->assertEquals('webauth.arizona.edu', $hostname);
+    $this->assertEquals('shibboleth.arizona.edu', $hostname);
   }
 
 }
