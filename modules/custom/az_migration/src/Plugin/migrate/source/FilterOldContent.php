e3a3394c2e12e72063e5f191f148d03db5ecd9dd
Lee Marsh <rmarsh1@arizona.edu>
2022-04-15 13:26:40 -0700

    Code standard fixes

--- modules/custom/az_migration/src/Plugin/migrate/source/FilterOldContent.php
+++ modules/custom/az_migration/src/Plugin/migrate/source/FilterOldContent.php
@@ -30,28 +30,28 @@ use Drupal\node\Plugin\migrate\source\d7\Node as D7Node;
  *   id = "filter_old_content"
  * )
  */
-class FilterOldContent extends D7Node
-{
+class FilterOldContent extends D7Node {
 
   /**
    * {@inheritdoc}
    */
-    public function query()
-    {
-        /**
-         * Filter date from migration script can be a
-         * string date or a UNIX timestamp so get
-         * it into correct format
-         */
-        if (is_int($this->configuration['filter_date'])) {
-            $date_cutoff = $this->configuration['filter_date'];
-        } else {
-            $date_cutoff = (new \DateTime($this->configuration['filter_date']))->getTimestamp();
-        }
-
-        $query = parent::query();
-        $query->condition('n.changed', $date_cutoff, '>');
-
-        return $query;
+  public function query() {
+    /*
+     * Filter date from migration script can be a
+     * string date or a UNIX timestamp so get
+     * it into correct format
+     */
+    if (is_int($this->configuration['filter_date'])) {
+      $date_cutoff = $this->configuration['filter_date'];
     }
+    else {
+      $date_cutoff = (new \DateTime($this->configuration['filter_date']))->getTimestamp();
+    }
+
+    $query = parent::query();
+    $query->condition('n.changed', $date_cutoff, '>');
+
+    return $query;
+  }
+
 }
