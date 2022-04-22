a8c4bb45c3c78478cee5d11b1da040962b80ab9e
Chris Green <chrisgreen@arizona.edu>
2022-04-22 11:53:11 -0700

    Closes #1498: Update eslint and prettier (#1499)

--- modules/custom/az_paragraphs/js/az_paragraphs_full_width.es6.js
+++ modules/custom/az_paragraphs/js/az_paragraphs_full_width.es6.js
@@ -2,23 +2,23 @@
   Drupal.behaviors.calculateScrollbarWidth = {
     attach: () => {
       document.documentElement.style.setProperty(
-        "--scrollbar-width",
-        `${window.innerWidth - document.documentElement.clientWidth}px`
+        '--scrollbar-width',
+        `${window.innerWidth - document.documentElement.clientWidth}px`,
       );
     },
   };
   Drupal.behaviors.azParagraphsPushSidebarDown = {
     attach: () => {
-      const contentRegion = document.getElementById("content");
+      const contentRegion = document.getElementById('content');
       const allFullWidthElements = document.querySelectorAll(
-        ".paragraph.full-width-background"
+        '.paragraph.full-width-background',
       );
       const lastFullWidthElement =
         allFullWidthElements[allFullWidthElements.length - 1];
       const contentRegionPosition = contentRegion.getBoundingClientRect();
       const style =
         allFullWidthElements[0].currentStyle ||
-        window.getComputedStyle(lastFullWidthElement, "");
+        window.getComputedStyle(lastFullWidthElement, '');
       const bottomMargin = style.marginBottom;
       const contentRegionTop = contentRegionPosition.top;
       const lastFullWidthElementPosition =
@@ -27,34 +27,34 @@
       const sidebarTopMargin =
         lastFullWidthElementBottom - contentRegionTop + bottomMargin;
       document.documentElement.style.setProperty(
-        "--sidebar-top-margin",
-        `${sidebarTopMargin}`
+        '--sidebar-top-margin',
+        `${sidebarTopMargin}`,
       );
     },
   };
   Drupal.behaviors.calculateFullWidthNegativeMargins = {
     attach: () => {
-      const contentRegion = document.getElementById("block-az-barrio-content");
+      const contentRegion = document.getElementById('block-az-barrio-content');
       const contentRegionPosition = contentRegion.getBoundingClientRect();
       const distanceFromLeft = contentRegionPosition.left;
       const distanceFromRight = contentRegionPosition.right;
       const negativeLeftMargin = 0 - distanceFromLeft;
       const negativeRightMargin = 0 - distanceFromRight;
       document.documentElement.style.setProperty(
-        "--full-width-left-distance",
-        `${negativeLeftMargin}px`
+        '--full-width-left-distance',
+        `${negativeLeftMargin}px`,
       );
       document.documentElement.style.setProperty(
-        "--full-width-right-distance",
-        `${negativeRightMargin}px`
+        '--full-width-right-distance',
+        `${negativeRightMargin}px`,
       );
     },
   };
-  document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener('DOMContentLoaded', () => {
     Drupal.behaviors.azParagraphsPushSidebarDown.attach();
     Drupal.behaviors.calculateFullWidthNegativeMargins.attach();
   });
-  window.addEventListener("resize", () => {
+  window.addEventListener('resize', () => {
     Drupal.behaviors.calculateFullWidthNegativeMargins.attach();
     Drupal.behaviors.calculateScrollbarWidth.attach();
     Drupal.behaviors.azParagraphsPushSidebarDown.attach();
