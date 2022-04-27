4d323c538cb091e1d507bafb4eed933f7ba4b4f0
Chris Green <chrisgreen@arizona.edu>
2022-04-27 16:23:13 -0700

    Update az_paragraphs_az_text_media_youtube.js

--- modules/custom/az_paragraphs/az_paragraphs_text_media/js/az_paragraphs_az_text_media_youtube.js
+++ modules/custom/az_paragraphs/az_paragraphs_text_media/js/az_paragraphs_az_text_media_youtube.js
@@ -50,7 +50,8 @@
                 controls: 0,
                 showinfo: 0,
                 rel: 0,
-                wmode: 'transparent'
+                wmode: 'transparent',
+                origin: window.location.href,
               },
               events: {
                 onReady: window.onPlayerReady,
