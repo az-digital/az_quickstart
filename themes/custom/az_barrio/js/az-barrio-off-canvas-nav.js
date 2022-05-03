a686031d6cf3a610bfdead73aeea5544d8767e09
Chris Green <chrisgreen@arizona.edu>
2022-05-03 09:43:33 -0700

    Fixing javascript after updating navbar id

--- themes/custom/az_barrio/js/az-barrio-off-canvas-nav.js
+++ themes/custom/az_barrio/js/az-barrio-off-canvas-nav.js
@@ -1,7 +1,7 @@
 (function ($, Drupal) {
-  Drupal.behaviors.myModuleBehavior = {
-    attach: function (context, settings) {
-      $('#navbarOffcanvasDemo').on('opened.az.offcanvasmenu', function (e) {
+  Drupal.behaviors.azBarrioOffCanvasNav = {
+    attach: function () {
+      $('.navbar-offcanvas').on('opened.az.offcanvasmenu', function (e) {
 	      if ($(e.target.ownerDocument.activeElement).attr('id') === 'jsAzSearch') {
           $('#block-az-barrio-offcanvas-searchform input').trigger('focus');
 	      }
