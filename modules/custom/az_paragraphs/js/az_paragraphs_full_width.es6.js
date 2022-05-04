b500542c4b0967d0e66bc524226489cea35b0e10
Chris Green <chrisgreen@arizona.edu>
2022-05-04 10:52:57 -0700

    Closes #1462 Optimize javascript behaviors for full width elements. (#1467)
    Co-authored-by: Joe Parsons <471936+joeparsons@users.noreply.github.com>

--- modules/custom/az_paragraphs/js/az_paragraphs_full_width.es6.js
+++ modules/custom/az_paragraphs/js/az_paragraphs_full_width.es6.js
@@ -1,62 +1,104 @@
+/**
+ * @file
+ * Provides helper functions to ensure proper display of full-width-paragraphs.
+ */
+
 ((Drupal, window, document) => {
-  Drupal.behaviors.calculateScrollbarWidth = {
+  /**
+   * Calculates scroll bar width if any and assigns the value to the
+   * `--scrollbar-width` CSS variable on the html element.
+   */
+  function calculateScrollbarWidth() {
+    document.documentElement.style.setProperty(
+      '--scrollbar-width',
+      `${window.innerWidth - document.documentElement.clientWidth}px`,
+    );
+  }
+
+  /**
+   * Calculates and sets margin required to push sidebars beneath the last
+   * full-width paragraph on the page.
+   *
+   * This function assigns values to the `--sidebar-top-margin` CSS variable on
+   * the `html` element.
+   */
+  function pushSidebarsDown() {
+    const contentRegion = document.querySelectorAll('main.main-content');
+    const allFullWidthElements = document.querySelectorAll(
+      '.paragraph.full-width-background',
+    );
+    const lastFullWidthElement =
+      allFullWidthElements[allFullWidthElements.length - 1];
+    const contentRegionPosition = contentRegion[0].getBoundingClientRect();
+    const style =
+      allFullWidthElements[0].currentStyle ||
+      window.getComputedStyle(lastFullWidthElement, '');
+    const bottomMargin = style.marginBottom;
+    const contentRegionTop = contentRegionPosition.top;
+    const lastFullWidthElementPosition =
+      lastFullWidthElement.getBoundingClientRect();
+    const lastFullWidthElementBottom = lastFullWidthElementPosition.bottom;
+    const sidebarTopMargin =
+      lastFullWidthElementBottom - contentRegionTop + bottomMargin;
+    document.documentElement.style.setProperty(
+      '--sidebar-top-margin',
+      `${sidebarTopMargin}`,
+    );
+  }
+
+  /**
+   * Calculates and sets negative margins required for full with backgrounds.
+   *
+   * This function assigns values to the --full-width-left-distance` and
+   * `--full-width-right-distance` CSS variables on the `html` element.
+   */
+  function calculateFullWidthNegativeMargins() {
+    const contentRegion = document.querySelectorAll('.block-system-main-block');
+    const contentRegionPosition = contentRegion[0].getBoundingClientRect();
+    const distanceFromLeft = contentRegionPosition.left;
+    const distanceFromRight = contentRegionPosition.right;
+    const negativeLeftMargin = 0 - distanceFromLeft;
+    const negativeRightMargin = 0 - distanceFromRight;
+    document.documentElement.style.setProperty(
+      '--full-width-left-distance',
+      `${negativeLeftMargin}px`,
+    );
+    document.documentElement.style.setProperty(
+      '--full-width-right-distance',
+      `${negativeRightMargin}px`,
+    );
+  }
+
+  /**
+   * Attaches the the functions defined in this file to the document.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   After the document loads, execute functions.
+   */
+  Drupal.behaviors.azParagraphsFullWidthElements = {
     attach: () => {
-      document.documentElement.style.setProperty(
-        '--scrollbar-width',
-        `${window.innerWidth - document.documentElement.clientWidth}px`,
-      );
+      calculateScrollbarWidth();
+      calculateFullWidthNegativeMargins();
+      pushSidebarsDown();
     },
   };
-  Drupal.behaviors.azParagraphsPushSidebarDown = {
-    attach: () => {
-      const contentRegion = document.getElementById('content');
-      const allFullWidthElements = document.querySelectorAll(
-        '.paragraph.full-width-background',
-      );
-      const lastFullWidthElement =
-        allFullWidthElements[allFullWidthElements.length - 1];
-      const contentRegionPosition = contentRegion.getBoundingClientRect();
-      const style =
-        allFullWidthElements[0].currentStyle ||
-        window.getComputedStyle(lastFullWidthElement, '');
-      const bottomMargin = style.marginBottom;
-      const contentRegionTop = contentRegionPosition.top;
-      const lastFullWidthElementPosition =
-        lastFullWidthElement.getBoundingClientRect();
-      const lastFullWidthElementBottom = lastFullWidthElementPosition.bottom;
-      const sidebarTopMargin =
-        lastFullWidthElementBottom - contentRegionTop + bottomMargin;
-      document.documentElement.style.setProperty(
-        '--sidebar-top-margin',
-        `${sidebarTopMargin}`,
-      );
-    },
-  };
-  Drupal.behaviors.calculateFullWidthNegativeMargins = {
-    attach: () => {
-      const contentRegion = document.getElementById('block-az-barrio-content');
-      const contentRegionPosition = contentRegion.getBoundingClientRect();
-      const distanceFromLeft = contentRegionPosition.left;
-      const distanceFromRight = contentRegionPosition.right;
-      const negativeLeftMargin = 0 - distanceFromLeft;
-      const negativeRightMargin = 0 - distanceFromRight;
-      document.documentElement.style.setProperty(
-        '--full-width-left-distance',
-        `${negativeLeftMargin}px`,
-      );
-      document.documentElement.style.setProperty(
-        '--full-width-right-distance',
-        `${negativeRightMargin}px`,
-      );
-    },
-  };
-  document.addEventListener('DOMContentLoaded', () => {
-    Drupal.behaviors.azParagraphsPushSidebarDown.attach();
-    Drupal.behaviors.calculateFullWidthNegativeMargins.attach();
-  });
+
+  /**
+   * Recalculates values for CSS variables on window resize.
+   */
   window.addEventListener('resize', () => {
-    Drupal.behaviors.calculateFullWidthNegativeMargins.attach();
-    Drupal.behaviors.calculateScrollbarWidth.attach();
-    Drupal.behaviors.azParagraphsPushSidebarDown.attach();
+    calculateScrollbarWidth();
+    calculateFullWidthNegativeMargins();
+    pushSidebarsDown();
+  });
+  /**
+   * Recalculates values for CSS variables when azVideoPlay fires.
+   */
+  window.addEventListener('azVideoPlay', () => {
+    calculateScrollbarWidth();
+    calculateFullWidthNegativeMargins();
+    pushSidebarsDown();
   });
 })(Drupal, this, this.document);
