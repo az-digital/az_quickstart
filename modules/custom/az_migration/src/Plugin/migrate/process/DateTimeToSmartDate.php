0764d3860b7dffe87c634e22123546f9b9563d98
Chris Green <chrisgreen@arizona.edu>
2022-05-11 12:49:01 -0700

    CS fixes

--- modules/custom/az_migration/src/Plugin/migrate/process/DateTimeToSmartDate.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/DateTimeToSmartDate.php
@@ -4,7 +4,6 @@ namespace Drupal\az_migration\Plugin\migrate\process;
 
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
@@ -57,7 +56,6 @@ class DateTimeToSmartDate extends ProcessPluginBase implements ContainerFactoryP
       );
     }
   }
-
 
   /**
    * {@inheritdoc}
