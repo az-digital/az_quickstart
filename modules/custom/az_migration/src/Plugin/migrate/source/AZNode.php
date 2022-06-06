d9e44dc151dbddea34f1e2fea32c66d2c9688da3
Lee Marsh <rmarsh1@arizona.edu>
2022-06-06 11:18:36 -0700

    Adding some additional documentation to make changes more clear.

--- modules/custom/az_migration/src/Plugin/migrate/source/AZNode.php
+++ modules/custom/az_migration/src/Plugin/migrate/source/AZNode.php
@@ -13,6 +13,22 @@ use Drupal\node\Plugin\migrate\source\d7\Node as D7Node;
  * Additions to D7Node:
  *  - alias: string containing the content's relative path alias beginning
  *    with '/'
+ *  - filter_date: Accepts a single value, either a numberic
+ *    value representing a UNIX timestamp or a string value
+ *    representing a calendar date in the YYYY-MM-DD format.
+ *    This is used as the "cutoff" date, or the oldest possible
+ *    date to accept content, based on the last update date.
+ *
+ * Examples:
+ * Consider a site with hundreds of pages, most of which is from before
+ * 2016. None of the older content has been looked at or updated in nearly
+ * a decade. How to easily cut that out of the migration:
+ * @code
+ *  source:
+ *    plugin: filter_old_content
+ *    node_type: uagc_page
+ *    filter_date: "2016-01-01"
+ * @endcode
  *
  * @MigrateSource(
  *   id = "az_node"
@@ -49,6 +65,11 @@ class AZNode extends D7Node {
    */
   public function query() {
     $query = parent::query();
+    /*
+     * Filter date from migration script can be a
+     * string date or a UNIX timestamp so get
+     * it into correct format
+     */
     if (array_key_exists("filter_date", $this->configuration)) {
       if (is_int($this->configuration['filter_date'])) {
         $date_cutoff = $this->configuration['filter_date'];
