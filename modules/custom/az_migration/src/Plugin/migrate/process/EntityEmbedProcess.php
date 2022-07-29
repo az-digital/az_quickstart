73e21684ffe4e66738d4c66a6ef837a1814496b3
Chris Green <chrisgreen@arizona.edu>
2022-07-29 13:55:40 -0700

    Add early return back

--- modules/custom/az_migration/src/Plugin/migrate/process/EntityEmbedProcess.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/EntityEmbedProcess.php
@@ -209,6 +209,9 @@ class EntityEmbedProcess extends ProcessPluginBase implements ContainerFactoryPl
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
 
+    if (strpos($value, '<drupal-entity ') === FALSE) {
+      return $value;
+    }
     $html5 = new HTML5(['disable_html_ns' => TRUE]);
     $dom_text = '<html><body>' . $value . '</body></html>';
     $dom = $html5->parse($dom_text);
