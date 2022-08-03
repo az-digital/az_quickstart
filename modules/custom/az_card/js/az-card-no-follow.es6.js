b5854e699364f8c91025079084c34abe71ee4d61
Chris Green <chrisgreen@arizona.edu>
2022-08-03 13:20:40 -0700

    Adding a different approach for the JS

---
+++ modules/custom/az_card/js/az-card-no-follow.es6.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Adds event listener and handler for cards in order prevent links from working
+ * under certain circumstances.
+ */
+
+ ((document) => {
+  /**
+   * Disables links for cards.
+   */
+  function noFollow(event) {
+    event.preventDefault();
+  }
+
+  /**
+   * Adds event listeners to card links.
+   */
+  const cards = document.querySelectorAll('.az-card-no-follow');
+  [...cards].forEach((card) => {
+    card.addEventListener('click', noFollow);
+  });
+
+})(this.document);
