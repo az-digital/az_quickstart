a6fd2f0bcd3b82b730e333981ad0b0dcd4c5b283
Chris Green <chrisgreen@arizona.edu>
2022-09-13 17:51:47 -0700

    Update menu block defaults

---
+++ modules/custom/az_block_types/az_custom_menu_block/src/Plugin/Block/AzCustomMenuBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\az_custom_menu_block\Plugin\Block;
+
+use Drupal\menu_block\Plugin\Block\MenuBlock;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides an extended Menu block.
+ *
+ * @Block(
+ *   id = "az_menu_block",
+ *   admin_label = @Translation("AZ Menu block"),
+ *   category = @Translation("AZ Menus"),
+ *   deriver = "Drupal\menu_block\Plugin\Derivative\MenuBlock",
+ *   forms = {
+ *     "settings_tray" = "\Drupal\system\Form\SystemMenuOffCanvasForm",
+ *   },
+ * )
+ */
+class AzCustomMenuBlock extends MenuBlock {
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $defaults = parent::defaultConfiguration();
+    $defaults['suggestion'] = strtr('vertical_pills', '-', '_');
+    return $defaults;
+  }
+}
+
