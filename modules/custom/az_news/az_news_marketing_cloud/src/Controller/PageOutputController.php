5ab0b6346b9516fcda9cd284743a02517d803a58
Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>
2023-04-26 13:14:21 -0400

    Latest updates

--- modules/custom/az_news/az_news_marketing_cloud/src/Controller/PageOutputController.php
+++ modules/custom/az_news/az_news_marketing_cloud/src/Controller/PageOutputController.php
@@ -1,25 +1,20 @@
 <?php
+
 namespace Drupal\az_news_marketing_cloud\Controller;
 
-class PageOutputController {
+use Drupal\Core\Controller\ControllerBase;
+
+class PageOutputController extends ControllerBase {
 
   /**
    * Returns a render-able array for a test page.
    */
   public function template() {
 
-    // Do something with your variables here.
-    $myText = 'This is not just a default text!';
-    $myNumber = 1;
-    $myArray = [1, 2, 3];
+    return array (
+      '#theme' => 'html__export__marketing_cloud',
+    );
 
-    return [
-      // Your theme hook name.
-      '#theme' => 'az_news_marketing_cloud',
-      // Your variables.
-      '#variable1' => $myText,
-      '#variable2' => $myNumber,
-      '#variable3' => $myArray,
-    ];
   }
+
 }
