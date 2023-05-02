93a39ced4755c1546e958ce2aa9d5633b32946aa
Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>
2023-05-02 12:30:48 -0400

    Latest updates

--- modules/custom/az_news/az_news_marketing_cloud/src/Controller/PageOutputController.php
+++ modules/custom/az_news/az_news_marketing_cloud/src/Controller/PageOutputController.php
@@ -7,39 +7,72 @@ namespace Drupal\az_news_marketing_cloud\Controller;
 // use Symfony\Component\HttpFoundation\Response;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\NodeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 // class PageOutputController extends EntityViewController {
 // 
   /**
    * Returns a render-able array for a test page.
    */
-  // public function view(EntityInterface $node) {
-    // $page = parent::view($node);
-    // \Drupal::routeMatch()->getParameter('node');
-    // return array (
-    //   '#theme' => 'html__export__marketing_cloud',
-    // ); 
-    // $html = \Drupal::service('renderer')->renderRoot($node);
-    // $response = new Response();
-    // $response->setContent($html);
-    // return $response;
-  // }
+// public function view(EntityInterface $node) {
+// $page = parent::view($node);
+// \Drupal::routeMatch()->getParameter('node');
+// return array (
+//   '#theme' => 'html__export__marketing_cloud',
+// ); 
+// $html = \Drupal::service('renderer')->renderRoot($node);
+// $response = new Response();
+// $response->setContent($html);
+// return $response;
+// }
 //
 // }
 
 
+// class PageOutputController extends ControllerBase
+// {
+
+//   public function render(NodeInterface $node)
+//   {
+//     $fields = [
+//       'title' => $node->getTitle(),
+//       'body' => $node->get('body')->value,
+//       'field_image' => $node->get('field_image')->entity->getUrl(),
+//     ];
+//     return [
+//       '#theme' => 'html__export__marketing_cloud',
+//       '#fields' => $fields,
+//     ];
+//   }
+// }
+
 class PageOutputController extends ControllerBase {
 
+  protected $entityTypeManager;
+
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
   public function render(NodeInterface $node) {
-    $fields = [
-      'title' => $node->getTitle(),
-      'body' => $node->get('body')->value,
-      'field_image' => $node->get('field_image')->entity->getUrl(),
-    ];
-    return [
+    $view_builder = $this->entityTypeManager->getViewBuilder('node');
+    $display = 'node.az_marketing_cloud_text_layout'; // The name of the view mode you want to use.
+    $build = $view_builder->view($node, $display);
+
+    // Create a render array for the custom template.
+    $output = [
       '#theme' => 'html__export__marketing_cloud',
-      '#fields' => $fields,
+      '#content' => $build,
     ];
+
+    return $output;
   }
 
 }
