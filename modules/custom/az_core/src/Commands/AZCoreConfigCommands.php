309264da11676f2cde46ef02633898d92f0ba6a5
Troy Dean <tadean@email.arizona.edu>
2023-06-27 17:13:11 -0700

    Fixes #1537 add distribution config export.

--- modules/custom/az_core/src/Commands/AZCoreConfigCommands.php
+++ modules/custom/az_core/src/Commands/AZCoreConfigCommands.php
@@ -8,6 +8,11 @@ use Drupal\config_provider\Plugin\ConfigCollector;
 use Drupal\az_core\Plugin\ConfigProvider\QuickstartConfigProvider;
 use Drupal\Core\Extension\ModuleExtensionList;
 use Drupal\Core\Extension\Exception\UnknownExtensionException;
+use Drupal\Core\Config\FileStorage;
+use Drupal\config_update\ConfigDiffer;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Config\StorageException;
+use Drupal\Component\Serialization\Yaml;
 
 /**
  * Add missing permissions from the AZ Quickstart profile to the active site.
@@ -22,6 +27,20 @@ class AZCoreConfigCommands extends DrushCommands {
   protected $configCollector;
 
   /**
+   * Drupal\config_update\ConfigDiffer definition.
+   *
+   * @var \Drupal\config_update\ConfigDiffer
+   */
+  protected $configDiffer;
+
+  /**
+   * Drupal\Core\Config\ConfigFactory definition.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $configFactory;
+
+  /**
    * Drupal\Core\Extension\ModuleExtensionList definition.
    *
    * @var \Drupal\Core\Extension\ModuleExtensionList
@@ -29,19 +48,35 @@ class AZCoreConfigCommands extends DrushCommands {
   protected $extensionLister;
 
   /**
+   * Drupal\Component\Serialization\Yaml definition.
+   *
+   * @var \Drupal\Component\Serialization\Yaml
+   */
+  protected $yamlSerialization;
+
+  /**
    * Constructs a new AZCoreConfigCommands object.
    *
    * This command class provides commands that deal with configuration
    * management of the distribution.
    *
+   * @param \Drupal\Core\Config\ConfigFactory $configFactory
+   *   The config.factory service.
    * @param \Drupal\config_provider\Plugin\ConfigCollector $configCollector
    *   The config_provider.collector service.
+   * @param \Drupal\config_update\ConfigDiffer $configDiffer
+   *   The config_update.config_diff service.
    * @param \Drupal\Core\Extension\ModuleExtensionList $extensionLister
    *   The extension.list.module service.
+   * @param \Drupal\Component\Serialization\Yaml $yamlSerialization
+   *   The serialization.yaml service.
    */
-  public function __construct(ConfigCollector $configCollector, ModuleExtensionList $extensionLister) {
+  public function __construct(ConfigFactory $configFactory, ConfigCollector $configCollector, ConfigDiffer $configDiffer, ModuleExtensionList $extensionLister, Yaml $yamlSerialization) {
+    $this->configFactory = $configFactory;
     $this->configCollector = $configCollector;
+    $this->configDiffer = $configDiffer;
     $this->extensionLister = $extensionLister;
+    $this->yamlSerialization = $yamlSerialization;
   }
 
   /**
@@ -99,6 +134,98 @@ class AZCoreConfigCommands extends DrushCommands {
     if ($permissionCount < 1) {
       $this->output()->writeln("No missing permissions found.");
     }
+  }
+
+  /**
+   * Rewrite changed distribution configuration files to modules.
+   *
+   * @param string $modules
+   *   Optional comma-delimited module machine names.
+   *
+   * @command az-core-config-export
+   */
+  public function exportDistributionConfiguration($modules = '') {
+    $extensions = $this->extensionLister->getList();
+    $arguments = [];
+    if (!empty($modules)) {
+      $arguments = explode(',', $modules);
+    }
+    foreach ($extensions as $key => $extension) {
+      // Only run for distribution extensions.
+      if (substr($key, 0, 3) !== "az_") {
+        continue;
+      }
+      // If argument list supplied, only run for given modules.
+      if (!empty($arguments) && !in_array($key, $arguments)) {
+        continue;
+      }
+      $this->output()->writeln(dt('@extension configuration:', [
+        '@extension' => $key,
+      ]));
+      $providers = $this->configCollector->getConfigProviders();
+      foreach ($providers as $provider) {
+        $dir = $provider->getDirectory();
+        // Only examine providers with a defined storage directory.
+        if (empty($dir)) {
+          continue;
+        }
+        // Find out which config items the module's storage has available.
+        $path = $extension->getPath() . DIRECTORY_SEPARATOR . $dir;
+        $storage = new FileStorage($path);
+        $all = $storage->listAll();
+        foreach ($all as $item) {
+          // Get the state of module configuration versus active configuration.
+          $original = $storage->read($item);
+          $active = $this->configFactory->get($item)->get();
+          // Remove site-specific identifiers.
+          unset($original['_core']);
+          unset($original['uuid']);
+          unset($active['_core']);
+          unset($active['uuid']);
+          // Diff the state of configuration to check for changes.
+          $diff = $this->configDiffer->diff($original, $active);
+          if (!$diff->isEmpty()) {
+
+            if ($this->io()->confirm(dt('    Update [@key/@dir] @item?', [
+              '@key' => $key,
+              '@dir' => $dir,
+              '@item' => $item,
+            ]))) {
+              try {
+                $storage->write($item, $active);
+              }
+              catch (StorageException $e) {
+                $this->output()->writeln(dt('    Failed to write @item', [
+                  '@item' => $item,
+                ]));
+              }
+            }
+          }
+          else {
+            $this->output()->writeln(dt('    -- unmodified -- @item', [
+              '@item' => $item,
+              '@dir' => $dir,
+            ]));
+          }
+        }
+      }
+    }
+
+  }
+
+  /**
+   * Fetch a single configuration item from active storage with hashes removed.
+   *
+   * @param string $item
+   *   Configuration item to export.
+   *
+   * @command az-core-config-export-single
+   */
+  public function exportConfigSingle($item) {
+    $config = $this->configFactory->get($item)->get();
+    unset($config['_core']);
+    unset($config['uuid']);
+    $this->output()->writeln($this->yamlSerialization->encode($config));
   }
 
 }
