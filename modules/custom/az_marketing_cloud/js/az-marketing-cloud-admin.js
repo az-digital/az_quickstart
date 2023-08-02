bc6888d649bd07fe73fd06d2a440e7cfaeb66ced
Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>
2023-08-02 13:51:47 -0400

    Closes #2327 - Initial Marketing Cloud integration (#2360)

---
+++ modules/custom/az_marketing_cloud/js/az-marketing-cloud-admin.js
@@ -0,0 +1,22 @@
+let copyLinks = document.querySelectorAll('.js-click-copy');
+const base_url = window.location.origin;
+
+function handleClick(event) {
+  if (event.type === "click") {
+    event.preventDefault();
+    let href = event.srcElement.getAttribute("href");
+    navigator.clipboard.writeText(base_url+href);
+    event.srcElement.classList.add("js-click-copy--copied");
+
+  } else {
+    return false;
+  }
+}
+
+copyLinks.forEach(element => (
+  element.addEventListener(
+    "click",
+    handleClick,
+    false
+  )
+));
