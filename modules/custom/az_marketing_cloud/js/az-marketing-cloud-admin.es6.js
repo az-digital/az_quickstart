2e5dfd0c722176926c35d6046d1bf40c3eaa4a6f
Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>
2023-09-11 12:14:47 -0400

    Update view filters

---
+++ modules/custom/az_marketing_cloud/js/az-marketing-cloud-admin.es6.js
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * Provides click to copy functionality.
+ */
+
+((Drupal, window, document) => {
+  /**
+   * Handles click events on the click to copy links.
+   *
+   * @param {Event} event
+   *  The event object.
+   * @return {boolean}
+   * Returns false if the event type is not click.  Otherwise, adds the link to
+   * the user's clipboard and adds a class to the link to indicate that it has been
+   * copied.
+   */
+  function handleClick(event) {
+    const baseUrl = window.location.origin;
+    if (event.type === 'click') {
+      event.preventDefault();
+      const href = event.srcElement.getAttribute('href');
+      navigator.clipboard.writeText(baseUrl + href);
+      event.srcElement.classList.add(
+        'js-click-copy--copied',
+        'action-link--icon-checkmark',
+      );
+    } else {
+      return false;
+    }
+  }
+
+  const copyLinks = document.querySelectorAll(
+    '.view-id-az_marketing_cloud.view-display-id-admin .views-field.views-field-view-node-1 a',
+  );
+
+  copyLinks.forEach((element) =>
+    element.addEventListener('click', handleClick, false),
+  );
+})(Drupal, this, this.document);
