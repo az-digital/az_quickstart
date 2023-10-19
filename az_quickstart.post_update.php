3d406e69ffd09388e2b7ee41d2e0fb5feb03d158
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2023-10-19 12:48:19 -0700

    Apply suggestions from code review
    Co-authored-by: tadean <51835841+tadean@users.noreply.github.com>

--- az_quickstart.post_update.php
+++ az_quickstart.post_update.php
@@ -14,17 +14,19 @@ use Drupal\Core\Entity\EntityStorageException;
  * Ensure legacy Quickstart block_content view is replaced with the Drupal core
  * version updated in 10.1.x.
  */
-function az_quicsktart_post_update_force_import_core_block_view(&$sandbox) {
+function az_quickstart_post_update_force_import_core_block_view(&$sandbox) {
   $config_to_import = 'views.view.block_content';
   $module_path = \Drupal::service('extension.list.module')->getPath('block_content');
   $config_storage = new FileStorage($module_path . '/config/optional');
   $entity_type = \Drupal::service('config.manager')->getEntityTypeIdByName($config_to_import);
   $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+  $id = $storage->getIDFromConfigName($config_to_import, $storage->getEntityType()->getConfigPrefix());
+  $active = $storage->load($id);
   $config_record = $config_storage->read($config_to_import);
 
   try {
     /** @var \Drupal\Core\Config\Entity\ConfigEntityStorageInterface $storage */
-    $entity = $storage->createFromStorageRecord($config_record);
+    $entity = $storage->updateFromStorageRecord($active, $config_record);
     $entity->save();
   }
   catch (EntityStorageException $e) {
