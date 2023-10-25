b3e42f2b5f8a01112525e9b72962e1bd09682473
Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>
2023-10-25 11:01:38 -0400

    Initial commit

--- modules/custom/az_marketing_cloud/src/Plugin/Action/CreateGroupAction.php
+++ modules/custom/az_marketing_cloud/src/Plugin/Action/CreateGroupAction.php
@@ -5,6 +5,7 @@ namespace Drupal\az_marketing_cloud\Plugin\Action;
 use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Plugin\PluginFormInterface;
 
 /**
  * Create a group.
@@ -12,32 +13,54 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
  * @Action(
  *   id = "az_marketing_cloud_create_group_action",
  *   label = @Translation("Create a group"),
- *   type = ""
+ *   type = "node",
+ *   confirm = TRUE
  * )
  */
-class CreateGroupAction extends ViewsBulkOperationsActionBase
-{
+class CreateGroupAction extends ViewsBulkOperationsActionBase {
 
   use StringTranslationTrait;
 
   /**
    * {@inheritdoc}
    */
-  public function execute($entity = NULL)
-  {
-    // Do some processing..
+  public function execute(ContentEntityInterface $entity = NULL) {
+    if (!$state = $entity->get('moderation_state')->getString()) {
+      return $this->t(':title  - can\'t change state',
+        [
+          ':title' => $entity->getTitle(),
+        ]
+      );
+    }
 
-    // Don't return anything for a default completion message, otherwise return translatable markup.
-    return $this->t('Some result');
+    switch ($state) {
+      case 'archived':
+      case 'draft':
+        $entity->set('moderation_state', 'published');
+        $entity->save();
+        break;
+    }
+
+    return $this->t(':title state changed to :state',
+      [
+        ':title' => $entity->getTitle(),
+        ':state' => $entity->get('moderation_state')->getString(),
+      ]
+    );
   }
-
+  
   /**
    * {@inheritdoc}
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE)
   {
-    // If certain fields are updated, access should be checked against them as well.
-    // @see Drupal\Core\Field\FieldUpdateActionBase::access().
-    return $object->access('update', $account, $return_as_object);
+    if ($object instanceof Node) {
+      $can_update = $object->access('update', $account, TRUE);
+      $can_edit = $object->access('edit', $account, TRUE);
+
+      return $can_edit->andIf($can_update)->isAllowed();
+    }
+
+    return FALSE;
   }
 }
