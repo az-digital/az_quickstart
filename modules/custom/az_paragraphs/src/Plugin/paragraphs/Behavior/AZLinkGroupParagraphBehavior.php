4852c676be14d5b0f83cc50fc5501cfab8c23e1f
Ashley Slay <aslay@arizona.edu>
2023-11-13 13:21:56 -0500

    Initial setup

---
+++ modules/custom/az_paragraphs/src/Plugin/paragraphs/Behavior/AZLinkGroupParagraphBehavior.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\az_paragraphs\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\paragraphs\ParagraphInterface;
+
+/**
+ * Provides a behavior for text with background.
+ *
+ * @ParagraphsBehavior(
+ *   id = "az_link_group",
+ *   label = @Translation("Quickstart Link Group Paragraph Behavior"),
+ *   description = @Translation("Provides class selection for link group."),
+ *   weight = 0
+ * )
+ */
+class AZLinkGroupParagraphBehavior extends AZDefaultParagraphsBehavior {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    \Drupal::logger('az_paragraphs_link_group')->info('AZLinkGroupParagraphBehavior loaded');
+
+    $config = $this->getSettings($paragraph);
+
+    // Link Group style
+    $form['group_style'] = [
+      '#title' => $this->t('Style'),
+      '#type' => 'select',
+      '#default_value' => $config['group_style'] ?? 'buttons',
+      '#description' => $this->t('Determines the style/behavior of your group of links.'),
+      '#options' => [
+        'buttons' => $this->t('Buttons'),
+        'dropdown' => $this->t('Dropdown'),
+        'list_group' => $this->t('List Group'),
+      ],
+    ];
+
+    if ($config['group_style'] == 'dropdown') {
+      
+      // Dropdown title
+      $form['dropdown_title'] = [
+        '#title' => $this->t('Dropdown Title'),
+        '#type' => 'text',
+        '#description' => $this->t('The title of your dropdown menu.'),
+      ];
+    }
+
+    elseif ($config['group_style'] == 'buttons') {
+      // Button color
+      $form['button_color'] = [
+        '#title' => $this->t('Button Color'),
+        '#type' => 'select',
+        '#options' => [
+          'btn-red' => $this->t('Red'),
+          'btn-blue' => $this->t('Blue'),
+          'btn-white' => $this->t('White'),
+          'btn-outline-red' => $this->t('Red Outline'),
+          'btn-outline-blue' => $this->t('Blue Outline'),
+          'btn-outline-white' => $this->t('White Outline'),
+        ],
+        '#default_value' => $config['button_color'] ?? 'btn-red',
+        '#description' => $this->t('<br><big><b>Important:</b></big> Site editors are responsible for accessibility and brand guideline considerations.<ul><li>To ensure proper color contrast, use the text color accessibility test at the bottom of the @arizona_bootstrap_color_docs_link.</li><li>For guidance on using the University of Arizona color palette, visit @ua_brand_colors_link.</li></ul>',
+        [
+          '@arizona_bootstrap_color_docs_link' => Link::fromTextAndUrl('Arizona Bootstrap color documentation', Url::fromUri('https://digital.arizona.edu/arizona-bootstrap/docs/2.0/getting-started/color-contrast/', ['attributes' => ['target' => '_blank']]))->toString(),
+          '@ua_brand_colors_link' => Link::fromTextAndUrl('brand.arizona.edu/applying-the-brand/colors', Url::fromUri('https://brand.arizona.edu/applying-the-brand/colors', ['attributes' => ['target' => '_blank']]))->toString(),
+        ]),
+      ];
+
+      // Button size
+      $form['button_size'] = [
+        '#title' => $this->t('Button Size'),
+        '#type' => 'select',
+        '#options' => [
+          'btn-lg' => $this->t('Large'),
+          'btn-sm' => $this->t('Small'),
+        ],
+      ];
+
+    }
+
+
+    parent::buildBehaviorForm($paragraph, $form, $form_state);
+
+    // This places the form fields on the content tab rather than behavior tab.
+    // Note that form is passed by reference.
+    // @see https://www.drupal.org/project/paragraphs/issues/2928759
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocess(&$variables) {
+    parent::preprocess($variables);
+
+    /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */
+    $paragraph = $variables['paragraph'];
+
+    // Get plugin configuration and save in vars for twig to use.
+    $config = $this->getSettings($paragraph);
+    $variables['link_group'] = $config;
+  }
+
+}
