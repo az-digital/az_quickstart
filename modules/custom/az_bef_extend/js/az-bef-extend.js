90706a0d0a1504e511a4d7d139f7d27158c977a2
Krys Flores <krystianflores@gmail.com>
2023-11-14 16:38:44 -0700

    added options to include vertical filter styles for bef. Not sure if its technically a plugin though.

---
+++ modules/custom/az_bef_extend/js/az-bef-extend.js
@@ -0,0 +1,17 @@
+// File: js/az-bef-extend.js
+(function (Drupal) {
+  Drupal.behaviors.myBehavior = {
+    attach: function (context, settings) {
+      // Find all 'li' elements within the context.
+      let listItems = context.querySelectorAll('li');
+      // Loop through each 'li' element.
+      listItems.forEach(function (li) {
+        // Check if 'li' has a 'ul' as a child.
+        if (li.querySelector('ul')) {
+          // Add a class to the 'li'.
+          li.classList.add('has-nested-ul');
+        }
+      });
+    }
+  };
+})(Drupal);
