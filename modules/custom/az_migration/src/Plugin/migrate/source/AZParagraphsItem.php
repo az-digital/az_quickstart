00227825b8f3bdfefca6736751feca777274adb4
Chris Green <chrisgreen@arizona.edu>
2023-11-29 14:00:58 -0700

    Close #2959 refactor AZParagraphsItem migrate source plugin.

--- modules/custom/az_migration/src/Plugin/migrate/source/AZParagraphsItem.php
+++ modules/custom/az_migration/src/Plugin/migrate/source/AZParagraphsItem.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\az_migration\Plugin\migrate\source;
 
 use Drupal\migrate\Row;
@@ -29,30 +31,18 @@ class AZParagraphsItem extends ParagraphsItem {
    * {@inheritdoc}
    */
   public function query() {
+    $query = parent::query();
+
     // @phpstan-ignore-next-line
     $this->allowArchivedParagraphs = \Drupal::config('az_migration.settings')->get('allow_archived_paragraphs');
 
-    $query = $this->select('paragraphs_item', 'p')
-      ->fields('p',
-        ['item_id',
-          'bundle',
-          'field_name',
-          'archived',
-          'bottom_spacing',
-          'view_mode',
-        ])
-      ->fields('pr', ['revision_id']);
-    $query->innerJoin('paragraphs_item_revision', 'pr', static::JOIN);
+    $query->addField('p', 'bottom_spacing', 'bottom_spacing');
+    $query->addField('p', 'view_mode', 'view_mode');
 
     if (!$this->allowArchivedParagraphs) {
-      // Omit archived (deleted or stale) paragraphs.
-      $query->condition('p.archived', 0);
+      $this->excludeArchivedParagraphs($query);
     }
-    // This configuration item may be set by a deriver to restrict the
-    // bundles retrieved.
-    if ($this->configuration['bundle']) {
-      $query->condition('p.bundle', $this->configuration['bundle']);
-    }
+
     return $query;
   }
 
@@ -60,65 +50,140 @@ class AZParagraphsItem extends ParagraphsItem {
    * {@inheritdoc}
    */
   public function fields() {
-    $fields = [
-      'item_id' => $this->t('The paragraph_item id'),
-      'revision_id' => $this->t('The paragraph_item revision id'),
-      'bundle' => $this->t('The paragraph bundle'),
-      'field_name' => $this->t('The paragraph field_name'),
+
+    return [
       'bottom_space' => $this->t('The paragraph Bottom Space'),
       'view_mode' => $this->t('The paragraph view mode'),
-    ];
-    return $fields;
+    ] + parent::fields();
+
   }
 
   /**
    * {@inheritdoc}
    */
   public function prepareRow(Row $row) {
-
-    // Get Item Id and revision Id of paragraph.
+    // Extract item_id and revision_id from the current row.
     $item_id = $row->getSourceProperty('item_id');
     $revision_id = $row->getSourceProperty('revision_id');
 
-    // Checking the field collection fields present in the paragraph.
-    if (!empty($row->getSourceProperty('field_collection_names'))) {
-      // Getting field collection - fields names from configuration.
-      $field_collection_field_names = explode(',', $row->getSourceProperty('field_collection_names'));
-      foreach ($field_collection_field_names as $field) {
+    // Process field collections associated with the paragraph.
+    $this->processFieldCollections($row, $item_id, $revision_id);
 
-        // Geting field collention values for the paragraph.
-        $field_collection_data = $this->getFieldValues('paragraphs_item', $field, $item_id, $revision_id);
-
-        // Get Field API field values for each field collection item.
-        $field_names = array_keys($this->getFields('field_collection_item', $field));
-
-        $field_collection_field_values = [];
-        foreach ($field_names as $field_collection_field_name) {
-          foreach ($field_collection_data as $delta => $field_collection_data_item) {
-            $field_collection_value = $this->getFieldValues(
-              'field_collection_item',
-              $field_collection_field_name,
-              $field_collection_data_item['value'],
-              $field_collection_data_item['revision_id']
-            );
-            foreach ($field_collection_value as $field_collection_value_item) {
-              $field_collection_field_values[$delta]['delta'] = $delta;
-              $field_collection_field_values[$delta][$field_collection_field_name][] = $field_collection_value_item;
-            }
-          }
-        }
-        ksort($field_collection_field_values);
-        $source_property_name = $field . '_values';
-        $row->setSourceProperty($source_property_name, $field_collection_field_values);
-
-      }
-    }
-
-    foreach (array_keys($this->getFields('paragraphs_item', $row->getSourceProperty('bundle'))) as $field) {
-      $row->setSourceProperty($field, $this->getFieldValues('paragraphs_item', $field, $item_id, $revision_id));
-    }
+    // Process individual fields of the paragraph.
+    $this->processParagraphFields($row, $item_id, $revision_id);
 
     return $row;
+  }
+
+  /**
+   * Processes field collections within a paragraph.
+   *
+   * Field collections are groups of fields that can be reused across different entities.
+   * This function iterates over each field collection defined in the source property
+   * and processes them individually.
+   *
+   * @param Row $row
+   *   The current row being processed.
+   * @param string|int $item_id
+   *   The item ID of the paragraph.
+   * @param string|int $revision_id
+   *   The revision ID of the paragraph.
+   */
+  private function processFieldCollections(Row $row, $item_id, $revision_id) {
+      // Check if there are field collections defined for the paragraph.
+      if (!empty($row->getSourceProperty('field_collection_names'))) {
+          // Extract field collection names and process each collection.
+          $field_collection_field_names = explode(',', $row->getSourceProperty('field_collection_names'));
+          foreach ($field_collection_field_names as $field) {
+              $this->processEachFieldCollection($row, $field, $item_id, $revision_id);
+          }
+      }
+  }
+
+  /**
+   * Processes each individual field collection.
+   *
+   * Retrieves and sets the values for a specific field collection.
+   *
+   * @param Row $row
+   *   The current row being processed.
+   * @param string $field
+   *   The field collection name.
+   * @param string|int $item_id
+   *   The item ID of the paragraph.
+   * @param string|int $revision_id
+   *   The revision ID of the paragraph.
+   */
+  private function processEachFieldCollection(Row $row, $field, $item_id, $revision_id) {
+      // Retrieve field collection data for the specific field.
+      $field_collection_data = $this->getFieldValues('paragraphs_item', $field, $item_id, $revision_id);
+      // Process and sort field collection values.
+      $field_collection_field_values = $this->getFieldCollectionValues($field_collection_data, $field);
+      ksort($field_collection_field_values);
+
+      // Set the processed values back into the row.
+      $row->setSourceProperty($field . '_values', $field_collection_field_values);
+  }
+
+  /**
+   * Retrieves and organizes field collection values.
+   *
+   * @param array $field_collection_data
+   *   The raw field collection data.
+   * @param string $field
+   *   The field collection name.
+   * @return array
+   *   The organized field collection values.
+   */
+  private function getFieldCollectionValues($field_collection_data, $field) {
+      $field_collection_field_values = [];
+      foreach ($field_collection_data as $delta => $field_collection_data_item) {
+          $field_collection_value = $this->getFieldValues(
+              'field_collection_item',
+              $field,
+              $field_collection_data_item['item_id'],
+              $field_collection_data_item['revision_id']
+          );
+          $field_collection_field_values[$delta] = $field_collection_value;
+      }
+
+      return $field_collection_field_values;
+  }
+
+  /**
+   * Processes paragraph fields.
+   *
+   * Iterates over all fields of a paragraph and sets their values in the row.
+   *
+   * @param Row $row
+   *   The current row being processed.
+   * @param string|int $item_id
+   *   The item ID of the paragraph.
+   * @param string|int $revision_id
+   *   The revision ID of the paragraph.
+   */
+  private function processParagraphFields(Row $row, $item_id, $revision_id) {
+      foreach (array_keys($this->getFields('paragraphs_item', $row->getSourceProperty('bundle'))) as $field) {
+          $row->setSourceProperty($field, $this->getFieldValues('paragraphs_item', $field, $item_id, $revision_id));
+      }
+  }
+
+  /**
+   * Modifies the query to exclude archived paragraphs.
+   *
+   * This method alters the existing query by adding a condition that filters out
+   * paragraphs marked as archived. In Drupal, 'archived' often means the paragraph
+   * is either deleted or not in active use, depending on the specific implementation.
+   * By adding this condition, only active, non-archived paragraphs are fetched by the query.
+   *
+   * @param \Drupal\Core\Database\Query\SelectInterface $query
+   *   The query object that is being built for fetching paragraph items. This object
+   *   is modified by reference.
+   */
+  private function excludeArchivedParagraphs($query) {
+      // Add a condition to the query to exclude rows where 'archived' is marked as 1 (true).
+      // In this context, a value of 0 in 'p.archived' means the paragraph is not archived.
+      $query->condition('p.archived', 0);
   }
 
 }
