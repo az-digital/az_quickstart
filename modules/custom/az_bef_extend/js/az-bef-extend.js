fb513fbf8fd4a072353222bd19ed8067c3853184
Krys Flores <krystianflores@gmail.com>
2023-12-06 11:48:16 -0700

    Havent kept this updated

--- modules/custom/az_bef_extend/js/az-bef-extend.js
+++ modules/custom/az_bef_extend/js/az-bef-extend.js
@@ -1,13 +1,9 @@
-(function (Drupal) {
+((Drupal) => {
   Drupal.behaviors.myBehavior = {
-    attach: function (context, settings) {
-      // Find all 'li' elements within the context.
-      let listItems = context.querySelectorAll('li');
-      // Loop through each 'li' element.
-      listItems.forEach(function (li) {
-        // Check if 'li' has a 'ul' as a child.
+    attach: (context, settings) => {
+      const listItems = context.querySelectorAll('li');
+      listItems.forEach((li) => {
         if (li.querySelector('ul')) {
-          // Add a class to the 'li'.
           li.classList.add('has-nested-ul');
         }
       });
