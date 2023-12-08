9c48864d2f005138e2178835e29e9be6b88bb79d
Chris Green <chrisgreen@arizona.edu>
2023-12-08 14:57:50 -0700

    Adding some more context

--- themes/custom/az_barrio/js/az-ajax.js
+++ themes/custom/az_barrio/js/az-ajax.js
@@ -8,9 +8,11 @@
    * Theme override of the ajax progress indicator to use Arizona Bootstrap
    * classes and place in center of screen with inline style.
    *
+   * See: https://digital.arizona.edu/arizona-bootstrap/docs/2.0/components/spinners
+   *
    * @return {string}
    *   The HTML markup for the throbber.
    */
   Drupal.theme.ajaxProgressIndicatorFullscreen = () =>
-    '<div class="position-fixed" style="z-index:1261;top:48.5%;left:49%;"><div class="spinner-border text-secondary" style="width: 3rem; height: 3rem;" role="status"><span class="sr-only">Loading...</span></div></div>';
+    '<div class="position-fixed" style="z-index:1261;top:48.5%;left:49%;"><div class="spinner-border text-info" style="width: 3rem; height: 3rem;" role="status"><span class="sr-only">Loading...</span></div></div>';
 })(Drupal);
