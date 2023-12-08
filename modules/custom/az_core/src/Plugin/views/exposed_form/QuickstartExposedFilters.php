71b032ac27225015ffad152040d652afd86b01e6
Dana Hertzberg <59745109+danahertzberg@users.noreply.github.com>
2023-12-08 15:20:51 -0500

    Issue/2275 (#3002)
    * CSS updates
    
    * Remove space below search
    
    * Added reset button and started collapse function
    
    * Add view
    
    * Update styles
    
    * Update themes/custom/az_barrio/templates/blocks/block--exposedformaz-page-by-categoryrow-with-filters.html.twig
    
    * Update CSS location
    
    * Updating css
    
    * Updating CSS
    
    * Update composer.json
    
    * Update az_barrio.libraries.yml
    
    * Updates from workshop
    
    * fix issues
    
    ---------
    
    Co-authored-by: Chris Green <chrisgreen@arizona.edu>

---
+++ modules/custom/az_core/src/Plugin/views/exposed_form/QuickstartExposedFilters.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\az_core\Plugin\views\exposed_form;
+
+use Drupal\better_exposed_filters\Plugin\views\exposed_form\BetterExposedFilters;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Exposed form plugin that provides a basic exposed form.
+ *
+ * @ingroup views_exposed_form_plugins
+ *
+ * @ViewsExposedForm(
+ *   id = "az_bef",
+ *   title = @Translation("Quickstart exposed filters style"),
+ *   help = @Translation("Better exposed filters with additional Quickstart styles.")
+ * )
+ */
+class QuickstartExposedFilters extends BetterExposedFilters {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function exposedFormAlter(&$form, FormStateInterface $form_state) {
+    parent::exposedFormAlter($form, $form_state);
+
+    // Attach Quickstart styles.
+    $form['#attached']['library'][] = 'az_core/az-bef-sidebar';
+    // Vertical style intended for sidebar use.
+    $form['#attributes']['class'][] = 'az-bef-vertical';
+  }
+
+}
