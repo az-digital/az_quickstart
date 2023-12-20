959efc75a09f6980b0a94c1cff5af22b773ebeea
bberndt-uaz <74572157+bberndt-uaz@users.noreply.github.com>
2023-12-19 23:41:39 -0700

    #3017 Deprecate the Block Content Permissions module and no longer install by default (#3020)
    Co-authored-by: Joe Parsons <471936+joeparsons@users.noreply.github.com>

--- az_quickstart.post_update.php
+++ az_quickstart.post_update.php
@@ -6,8 +6,10 @@
  */
 
 use Drupal\Component\Utility\Crypt;
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
 use Drupal\Core\Config\FileStorage;
 use Drupal\Core\Entity\EntityStorageException;
+use Drupal\user\Entity\Role;
 
 /**
  * Force import of core block_content view.
@@ -40,4 +42,30 @@ function az_quickstart_post_update_force_import_core_block_view(&$sandbox) {
   catch (EntityStorageException $e) {
     \Drupal::logger('az_quickstart')->notice($e->getMessage());
   }
+}
+
+/**
+ * Remove unneeded block_content permissions (core + contrib) from AZQS roles.
+ */
+function az_quickstart_post_update_remove_block_content_permissions_from_roles(&$sandbox) {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'user_role', function (Role $role) {
+    $update = FALSE;
+    if (in_array($role->get('id'), ['az_content_admin', 'az_content_editor'])) {
+      $permissions_to_remove = [
+        'administer block content types',
+        'administer block types',
+        'update any az_custom_menu_block block content',
+        'update any az_flexible_block block content',
+        'update any az_quick_links block content',
+        'view restricted block content',
+      ];
+      foreach ($permissions_to_remove as $permission) {
+        if ($role->hasPermission($permission)) {
+          $role->revokePermission($permission);
+          $update = TRUE;
+        }
+      }
+    }
+    return $update;
+  });
 }
