1176fcc8d8b98e473f56e56f209ff49f0de51c52
Chris Green <chrisgreen@arizona.edu>
2024-02-14 09:23:17 -0700

    more updates

--- modules/custom/az_publication/src/Form/AZPublicationTypeForm.php
+++ modules/custom/az_publication/src/Form/AZPublicationTypeForm.php
@@ -65,7 +65,7 @@ class AZPublicationTypeForm extends EntityForm {
       '#type' => 'radios',
       '#title' => $this->t('Publication Type Mapping'),
       '#options' => $az_publication_type->getMappableTypeOptions(),
-      '#default_value' => $az_publication_type->getType(),
+      '#default_value' => $az_publication_type->get('type'),
       '#description' => $this->t('Select the type of publication this entry corresponds to. Each type aligns with the standards defined in the Citation Style Language (CSL). Your selection here will determine how the publication is formatted and displayed. For a detailed explanation of each type, refer to the CSL documentation: <a href=":csl-docs" target="_blank">CSL Types Specification</a>.', [
         ':csl-docs' => 'https://docs.citationstyles.org/en/stable/specification.html#appendix-iii-types',
       ]),
