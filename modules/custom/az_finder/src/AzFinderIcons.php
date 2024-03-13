84d41107adc6f00a5e1be649728a3ddeddeca0ef
Chris Green <chrisgreen@arizona.edu>
2024-03-13 14:30:04 -0700

    Update modules/custom/az_finder/src/AzFinderIcons.php
    Co-authored-by: Joe Parsons <471936+joeparsons@users.noreply.github.com>

--- modules/custom/az_finder/src/AzFinderIcons.php
+++ modules/custom/az_finder/src/AzFinderIcons.php
@@ -7,7 +7,7 @@ namespace Drupal\az_finder;
 /**
  * Provides SVG icons for the Quickstart Finder module.
  */
-final class AZFinderIcons {
+final class AzFinderIcons {
 
   /**
    * Cache for SVG icon render arrays.
