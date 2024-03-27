3d279822856a8163473e1736f6314be77f9f6888
Chris Green <chrisgreen@arizona.edu>
2024-03-27 14:35:18 -0700

    Move reset buttom finder into drupal behavior

--- modules/custom/az_finder/js/active-filter-reset.js
+++ modules/custom/az_finder/js/active-filter-reset.js
@@ -5,32 +5,32 @@
 * @preserve
 **/
 (function (Drupal) {
-  var clickHandler = function clickHandler(selector, action) {
-    var element = selector.querySelector('.js-form-submit');
-    if (element && action) action(element);
-  };
-  var resetFilters = function resetFilters(container) {
-    var checkboxes = container.querySelectorAll('input[type="checkbox"]');
-    var textFields = container.querySelectorAll('input[type="text"]');
-    checkboxes.forEach(function (checkbox) {
-      checkbox.checked = false;
-    });
-    textFields.forEach(function (textField) {
-      textField.value = '';
-    });
-    clickHandler(container, function (element) {
-      return element.click();
-    });
-    var event = new CustomEvent('az-finder-filter-reset', {
-      bubbles: true,
-      detail: {
-        message: 'Filters have been reset.'
-      }
-    });
-    container.dispatchEvent(event);
-  };
   Drupal.behaviors.azFinderActiveFilterReset = {
     attach: function attach(context) {
+      var clickHandler = function clickHandler(selector, action) {
+        var element = selector.querySelector('.js-form-submit');
+        if (element && action) action(element);
+      };
+      var resetFilters = function resetFilters(container) {
+        var checkboxes = container.querySelectorAll('input[type="checkbox"]');
+        var textFields = container.querySelectorAll('input[type="text"]');
+        checkboxes.forEach(function (checkbox) {
+          checkbox.checked = false;
+        });
+        textFields.forEach(function (textField) {
+          textField.value = '';
+        });
+        clickHandler(container, function (element) {
+          return element.click();
+        });
+        var event = new CustomEvent('az-finder-filter-reset', {
+          bubbles: true,
+          detail: {
+            message: 'Filters have been reset.'
+          }
+        });
+        container.dispatchEvent(event);
+      };
       context.querySelectorAll('[data-az-better-exposed-filters]').forEach(function (container) {
         var resetButton = container.querySelector('.js-active-filters-reset');
         if (resetButton) {
