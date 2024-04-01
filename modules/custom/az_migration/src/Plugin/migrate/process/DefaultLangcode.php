711d00d3bda9f779c193c3d4c9b38e4ba521daf5
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2024-04-01 12:27:02 -0700

    Update modules/custom/az_migration/src/Plugin/migrate/process/DefaultLangcode.php
    Co-authored-by: Brian Berndt <74572157+bberndt-uaz@users.noreply.github.com>

--- modules/custom/az_migration/src/Plugin/migrate/process/DefaultLangcode.php
+++ modules/custom/az_migration/src/Plugin/migrate/process/DefaultLangcode.php
@@ -59,7 +59,7 @@ class DefaultLangcode extends ProcessPluginBase {
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
     return new static(
       $configuration,
       $plugin_id,
