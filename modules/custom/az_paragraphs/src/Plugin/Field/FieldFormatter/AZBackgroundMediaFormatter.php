26c9a45fac90e8001ab8924612338f0d118b1830
Joe Parsons <parsonsj@arizona.edu>
2024-05-13 13:59:45 -0700

    PHPCBF.

--- modules/custom/az_paragraphs/src/Plugin/Field/FieldFormatter/AZBackgroundMediaFormatter.php
+++ modules/custom/az_paragraphs/src/Plugin/Field/FieldFormatter/AZBackgroundMediaFormatter.php
@@ -63,11 +63,11 @@ class AZBackgroundMediaFormatter extends EntityReferenceFormatterBase implements
    * {@inheritdoc}
    */
   public static function create(
-      ContainerInterface $container,
-      array $configuration,
-      $plugin_id,
-      $plugin_definition
-    ) {
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) {
     $instance = parent::create(
       $container,
       $configuration,
