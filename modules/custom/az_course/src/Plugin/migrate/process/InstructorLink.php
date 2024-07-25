115e8fdc22752a04d4c6f8bb67cad344ddb7a078
Troy Dean <tadean@arizona.edu>
2024-07-25 16:54:07 -0700

    Fixes #3568 Course Instructor Person Links

--- modules/custom/az_course/src/Plugin/migrate/process/InstructorLink.php
+++ modules/custom/az_course/src/Plugin/migrate/process/InstructorLink.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\az_course\Plugin\migrate\process;
 
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\migrate\Attribute\MigrateProcess;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns a link to a user, or a non-link.
@@ -13,22 +15,51 @@ use Drupal\migrate\Row;
  * @see \Drupal\migrate\Plugin\MigrateProcessInterface
  */
 #[MigrateProcess('instructor_link')]
-class InstructorLink extends ProcessPluginBase {
+class InstructorLink extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition
+    );
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
 
-    $nolink = ['uri' => 'route:<nolink>', 'title' => $value];
+    $link = ['uri' => 'route:<nolink>', 'title' => $value];
 
     // Placeholder. Check for presence of instructor when field is available
     // to create a link to faculty pages.
     if ($value === 'netid-not-found') {
-      $nolink['title'] = 'unassigned';
+      $link['title'] = 'unassigned';
+    }
+    else {
+      // See if there is a person with a matching netid.
+      $persons = $this->entityTypeManager->getStorage('node')->loadByProperties([
+        'field_az_netid' => $value,
+        'status' => [1, TRUE],
+      ]);
+      if (!empty($persons)) {
+        $person = reset($persons);
+        $link['uri'] = 'entity:node/' . $person->id();
+      }
     }
 
-    return $nolink;
+    return $link;
   }
 
 }
