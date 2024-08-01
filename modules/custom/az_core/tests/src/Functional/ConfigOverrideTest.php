20430e9c131c76ddb4f306706ad23f9e77f4e06c
Chris Green <chrisgreen@arizona.edu>
2024-08-01 15:40:49 -0700

    Close #3588 Add PHPUnit Test on 2.9.x branch

--- modules/custom/az_core/tests/src/Functional/ConfigOverrideTest.php
+++ modules/custom/az_core/tests/src/Functional/ConfigOverrideTest.php
@@ -51,4 +51,32 @@ class ConfigOverrideTest extends BrowserTestBase {
     $this->assertEquals('shibboleth.arizona.edu', $hostname);
   }
 
+  /**
+   * Tests a config override was applied successfully after module install.
+   */
+  public function testConfigOverrideAfterInstall() {
+    // Install the az_mail module.
+    $this->container->get('module_installer')->install(['az_mail']);
+    $config = $this->config('smtp.settings');
+    $this->assertEquals(TRUE, $config->get('smtp_on'));
+    $this->assertEquals('email-smtp.us-west-2.amazonaws.com', $config->get('smtp_host'));
+    $this->assertEquals('', $config->get('smtp_hostbackup'));
+    $this->assertEquals('2587', $config->get('smtp_port'));
+    $this->assertEquals('tls', $config->get('smtp_protocol'));
+    $this->assertEquals(TRUE, $config->get('smtp_autotls'));
+    $this->assertEquals(30, $config->get('smtp_timeout'));
+    $this->assertEquals('', $config->get('smtp_username'));
+    $this->assertEquals('', $config->get('smtp_password'));
+    $this->assertEquals('', $config->get('smtp_from'));
+    $this->assertEquals('', $config->get('smtp_fromname'));
+    $this->assertEquals('', $config->get('smtp_client_hostname'));
+    $this->assertEquals('', $config->get('smtp_client_helo'));
+    $this->assertEquals('0', $config->get('smtp_allowhtml'));
+    $this->assertEquals('', $config->get('smtp_test_address'));
+    $this->assertEquals('', $config->get('smtp_reroute_address'));
+    $this->assertEquals(FALSE, $config->get('smtp_debugging'));
+    $this->assertEquals('php_mail', $config->get('prev_mail_system'));
+    $this->assertEquals(FALSE, $config->get('smtp_keepalive'));
+  }
+
 }
