692bf23b8f787f58d3c6537e403309827c01f75c
Chris Green <chrisgreen@arizona.edu>
2024-08-02 13:15:59 -0700

    Update js code

--- modules/custom/az_zoom/js/az-zoom.drupal.es6.js
+++ modules/custom/az_zoom/js/az-zoom.drupal.es6.js
@@ -1,26 +1,30 @@
-// @file az-zoom.drupal.js
-
+/**
+ * @file
+ * az-zoom.drupal.js
+ *
+ * This file contains the JavaScript needed to create a zoom effect on images.
+ */
 import FocusImg from './az-zoom.js';
 
-((Drupal, drupalSettings) => {
+((drupalSettings, Drupal) => {
   Drupal.behaviors.zoomEffect = {
-    attach: function (context, drupalSettings) {
+    attach(context, settings) {
       context.querySelectorAll('.image-zoom-container').forEach((element) => {
         // Find the <img> tag within the .image-zoom-container
-        let imgElement = element.querySelector('.original-image');
-        let fid = imgElement.getAttribute('fid');
+        const imgElement = element.querySelector('.original-image');
+        const fid = imgElement.getAttribute('fid');
         new FocusImg({
-          imageSrc: drupalSettings.AZZoom.image_urls[fid] || imgElement.src,
+          imageSrc: settings.AZZoom.image_urls[fid] || imgElement.src,
           parentElement: element,
-          zoomFactor: drupalSettings.AZZoom.image_zoom_factor || '250%',
-          smoother: drupalSettings.AZZoom.image_smoother || true,
-          width: drupalSettings.AZZoom.image_width || '100%',
-          height: drupalSettings.AZZoom.image_height || '66.7%',
-          displayLoc: drupalSettings.AZZoom.display_loc || false,
-          displayZoom: drupalSettings.AZZoom.display_zoom || false,
-          zoomOnScroll: drupalSettings.AZZoom.zoom_on_scroll || false,
+          zoomFactor: settings.AZZoom.image_zoom_factor || '250%',
+          smoother: settings.AZZoom.image_smoother || true,
+          width: settings.AZZoom.image_width || '100%',
+          height: settings.AZZoom.image_height || '66.7%',
+          displayLoc: settings.AZZoom.display_loc || false,
+          displayZoom: settings.AZZoom.display_zoom || false,
+          zoomOnScroll: settings.AZZoom.zoom_on_scroll || false,
         });
       });
     },
   };
-})(Drupal, drupalSettings);
+})(drupalSettings, Drupal);
--- modules/custom/az_zoom/js/az-zoom.drupal.js
+++ modules/custom/az_zoom/js/az-zoom.drupal.js
@@ -5,24 +5,24 @@
 * @preserve
 **/
 import FocusImg from './az-zoom.js';
-(function (Drupal, drupalSettings) {
+(function (drupalSettings, Drupal) {
   Drupal.behaviors.zoomEffect = {
-    attach: function attach(context, drupalSettings) {
+    attach: function attach(context, settings) {
       context.querySelectorAll('.image-zoom-container').forEach(function (element) {
         var imgElement = element.querySelector('.original-image');
         var fid = imgElement.getAttribute('fid');
         new FocusImg({
-          imageSrc: drupalSettings.AZZoom.image_urls[fid] || imgElement.src,
+          imageSrc: settings.AZZoom.image_urls[fid] || imgElement.src,
           parentElement: element,
-          zoomFactor: drupalSettings.AZZoom.image_zoom_factor || '250%',
-          smoother: drupalSettings.AZZoom.image_smoother || true,
-          width: drupalSettings.AZZoom.image_width || '100%',
-          height: drupalSettings.AZZoom.image_height || '66.7%',
-          displayLoc: drupalSettings.AZZoom.display_loc || false,
-          displayZoom: drupalSettings.AZZoom.display_zoom || false,
-          zoomOnScroll: drupalSettings.AZZoom.zoom_on_scroll || false
+          zoomFactor: settings.AZZoom.image_zoom_factor || '250%',
+          smoother: settings.AZZoom.image_smoother || true,
+          width: settings.AZZoom.image_width || '100%',
+          height: settings.AZZoom.image_height || '66.7%',
+          displayLoc: settings.AZZoom.display_loc || false,
+          displayZoom: settings.AZZoom.display_zoom || false,
+          zoomOnScroll: settings.AZZoom.zoom_on_scroll || false
         });
       });
     }
   };
-})(Drupal, drupalSettings);
+})(drupalSettings, Drupal);
