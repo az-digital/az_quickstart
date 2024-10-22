836c2885ea731391deee40d36717f499c1b7299f
Chris Green <chrisgreen@arizona.edu>
2024-10-21 17:10:35 -0700

    Update AZPublicationTypeForm.php

--- modules/custom/az_publication/src/Form/AZPublicationTypeForm.php
+++ modules/custom/az_publication/src/Form/AZPublicationTypeForm.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\az_publication\Form;
 
-use Drupal\az_publication\Entity\AZPublicationType;
 use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\az_publication\Entity\AZPublicationType;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
