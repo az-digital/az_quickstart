393aef935e8632c01c4a13588e58892f3bc94841
Chris Green <chrisgreen@arizona.edu>
2024-12-12 09:59:24 -0700

    Check if field exists on person entity

--- modules/custom/az_person/src/AZUserToolbarLinkBuilder.php
+++ modules/custom/az_person/src/AZUserToolbarLinkBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\az_person;
 
+use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Url;
@@ -14,18 +15,25 @@ use Drupal\user\ToolbarLinkBuilder;
 class AZUserToolbarLinkBuilder extends ToolbarLinkBuilder {
 
   /**
-   * Drupal\externalauth\AuthmapInterface definition.
+   * Authmap service definition.
    *
    * @var \Drupal\externalauth\AuthmapInterface
    */
   protected $authmap;
 
   /**
-   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   * Entity type manager service definition.
    *
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
+
+  /**
+   * Entity field manager service definition.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
 
   /**
    * ToolbarHandler constructor.
@@ -34,12 +42,20 @@ class AZUserToolbarLinkBuilder extends ToolbarLinkBuilder {
    *   The current user.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entityFieldManager
+   *   The entity field manager.
    * @param \Drupal\externalauth\AuthmapInterface $authmap
    *   The authmap service.
    */
-  public function __construct(AccountProxyInterface $account, EntityTypeManagerInterface $entityTypeManager, ?AuthmapInterface $authmap) {
+  public function __construct(
+    AccountProxyInterface $account,
+    EntityTypeManagerInterface $entityTypeManager,
+    EntityFieldManagerInterface $entityFieldManager,
+    ?AuthmapInterface $authmap
+  ) {
     parent::__construct($account);
     $this->entityTypeManager = $entityTypeManager;
+    $this->entityFieldManager = $entityFieldManager;
     $this->authmap = $authmap;
   }
 
@@ -52,41 +68,50 @@ class AZUserToolbarLinkBuilder extends ToolbarLinkBuilder {
   public function renderToolbarLinks() {
     $build = parent::renderToolbarLinks();
     $additional_links = [];
+
     // Only valid if we have the externalauth module.
     if (!empty($this->authmap)) {
       // Check if we have permission.
       if ($this->account->hasPermission('edit matching netid content')) {
         $auth = $this->authmap->get($this->account->id(), 'cas');
-        if (($auth !== FALSE)) {
-          // Check if we have a linked person.
-          $persons = $this->entityTypeManager->getStorage('node')->loadByProperties([
-            'field_az_netid' => $auth,
-            'type' => 'az_person',
-            'status' => [1, TRUE],
-          ]);
-          if (!empty($persons)) {
-            $person = reset($persons);
-            // If we have a linked az person, generate links.
-            $additional_links = [
-              'az_person' => [
-                'title' => $this->t('View my web page'),
-                'url' => Url::fromRoute('entity.node.canonical', ['node' => $person->id()]),
-                'attributes' => [
+
+        if ($auth !== FALSE && is_string($auth)) {
+          // Verify that 'field_az_netid' exists for 'az_person' content type.
+          $field_definitions = $this->entityFieldManager->getFieldDefinitions('node', 'az_person');
+
+          if (isset($field_definitions['field_az_netid'])) {
+            // Check if we have a linked person.
+            $persons = $this->entityTypeManager->getStorage('node')->loadByProperties([
+              'field_az_netid' => $auth,
+              'type' => 'az_person',
+              'status' => [1, TRUE],
+            ]);
+
+            if (!empty($persons)) {
+              $person = reset($persons);
+              // If we have a linked az person, generate links.
+              $additional_links = [
+                'az_person' => [
                   'title' => $this->t('View my web page'),
+                  'url' => Url::fromRoute('entity.node.canonical', ['node' => $person->id()]),
+                  'attributes' => [
+                    'title' => $this->t('View my web page'),
+                  ],
                 ],
-              ],
-              'az_person_edit' => [
-                'title' => $this->t('Edit my web page'),
-                'url' => Url::fromRoute('entity.node.edit_form', ['node' => $person->id()]),
-                'attributes' => [
+                'az_person_edit' => [
                   'title' => $this->t('Edit my web page'),
+                  'url' => Url::fromRoute('entity.node.edit_form', ['node' => $person->id()]),
+                  'attributes' => [
+                    'title' => $this->t('Edit my web page'),
+                  ],
                 ],
-              ],
-            ];
+              ];
+            }
           }
         }
       }
     }
+
     // Transform some of the user module links for clarity.
     if (!empty($build['#links'])) {
       $original_links = $build['#links'];
@@ -100,6 +125,7 @@ class AZUserToolbarLinkBuilder extends ToolbarLinkBuilder {
       $links = array_merge($additional_links, $original_links);
       $build['#links'] = $links;
     }
+
     return $build;
   }
 
