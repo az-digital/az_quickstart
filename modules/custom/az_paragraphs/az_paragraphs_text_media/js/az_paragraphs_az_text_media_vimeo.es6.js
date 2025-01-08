162ac165479ce0c46f4c866cc6bdbbe3fc70447d
BaoNguyen09 <thienbao05@arizona.edu>
2025-01-08 13:32:04 -0700

    fix: ensure vimeo api is loaded before proceeding

--- modules/custom/az_paragraphs/az_paragraphs_text_media/js/az_paragraphs_az_text_media_vimeo.es6.js
+++ modules/custom/az_paragraphs/az_paragraphs_text_media/js/az_paragraphs_az_text_media_vimeo.es6.js
@@ -25,64 +25,67 @@
           document.head.appendChild(tag);
   
           // Methods
-          $.each(bgVideoParagraphs, index => {
-            const thisContainer = bgVideoParagraphs[index];
-            const parentParagraph = thisContainer.parentNode;
-            const vimeoId = thisContainer.dataset.vimeoId;
-            bgVideos[vimeoId] = $.extend({}, defaults, thisContainer);
-            const options = bgVideos[vimeoId];
-            const videoPlayer = thisContainer.getElementsByClassName(
-              "az-video-player"
-            )[0];
-            const VimeoPlayer = window.Vimeo.Player;
-  
-            // Initialize Vimeo Player
-            thisContainer.player = new VimeoPlayer(videoPlayer, {
-              id: vimeoId,
-              width: options.width,
-              height: Math.ceil(options.width / options.ratio),
-              autoplay: true,
-              muted: options.mute,
-              loop: options.repeat
-            });
-  
-            // Event Listeners
-            thisContainer.player.on("play", () => {
-              parentParagraph.classList.add("az-video-playing");
-              parentParagraph.classList.remove("az-video-paused");
-            });
-  
-            thisContainer.player.on("pause", () => {
-              parentParagraph.classList.add("az-video-paused");
-              parentParagraph.classList.remove("az-video-playing");
-            });
-  
-            thisContainer.player.on("ended", () => {
-              if (options.repeat) {
-                thisContainer.player.setCurrentTime(0).then(() => {
-                  thisContainer.player.play();
+          // Ensure Vimeo API is loaded before proceeding
+          tag.onload = () => {
+            $.each(bgVideoParagraphs, index => {
+                const thisContainer = bgVideoParagraphs[index];
+                const parentParagraph = thisContainer.parentNode;
+                const vimeoId = thisContainer.dataset.vimeoId;
+                bgVideos[vimeoId] = $.extend({}, defaults, thisContainer);
+                const options = bgVideos[vimeoId];
+                const videoPlayer = thisContainer.getElementsByClassName(
+                  "az-video-player"
+                )[0];
+                const VimeoPlayer = window.Vimeo;
+      
+                // Initialize Vimeo Player
+                thisContainer.player = new VimeoPlayer.Player(videoPlayer, {
+                  id: vimeoId,
+                  width: options.width,
+                  height: Math.ceil(options.width / options.ratio),
+                  autoplay: true,
+                  muted: options.mute,
+                  loop: options.repeat
                 });
-              }
-            });
-  
-            // Play Button
-            const playButton = bgVideoParagraphs[index].getElementsByClassName(
-              "az-video-play"
-            )[0];
-            playButton.addEventListener("click", event => {
-              event.preventDefault();
-              bgVideoParagraphs[index].player.play();
-            });
-  
-            // Pause Button
-            const pauseButton = bgVideoParagraphs[index].getElementsByClassName(
-              "az-video-pause"
-            )[0];
-            pauseButton.addEventListener("click", event => {
-              event.preventDefault();
-              bgVideoParagraphs[index].player.pause();
-            });
-          });
+      
+                // Event Listeners
+                thisContainer.player.on("play", () => {
+                  parentParagraph.classList.add("az-video-playing");
+                  parentParagraph.classList.remove("az-video-paused");
+                });
+      
+                thisContainer.player.on("pause", () => {
+                  parentParagraph.classList.add("az-video-paused");
+                  parentParagraph.classList.remove("az-video-playing");
+                });
+      
+                thisContainer.player.on("ended", () => {
+                  if (options.repeat) {
+                    thisContainer.player.setCurrentTime(0).then(() => {
+                      thisContainer.player.play();
+                    });
+                  }
+                });
+      
+                // Play Button
+                const playButton = bgVideoParagraphs[index].getElementsByClassName(
+                  "az-video-play"
+                )[0];
+                playButton.addEventListener("click", event => {
+                  event.preventDefault();
+                  bgVideoParagraphs[index].player.play();
+                });
+      
+                // Pause Button
+                const pauseButton = bgVideoParagraphs[index].getElementsByClassName(
+                  "az-video-pause"
+                )[0];
+                pauseButton.addEventListener("click", event => {
+                  event.preventDefault();
+                  bgVideoParagraphs[index].player.pause();
+                });
+              });
+          }
   
           // Resize Logic (optional, similar to YouTube)
           const resize = () => {
@@ -91,6 +94,7 @@
               const videoPlayer = thisContainer.getElementsByClassName(
                 "az-video-player"
               )[0];
+              const vimeoId = thisContainer.dataset.vimeoId;
               const { ratio } = bgVideos[vimeoId];
               const width = thisContainer.offsetWidth;
               const height = Math.ceil(width / ratio);
--- modules/custom/az_paragraphs/az_paragraphs_text_media/js/az_paragraphs_az_text_media_vimeo.js
+++ modules/custom/az_paragraphs/az_paragraphs_text_media/js/az_paragraphs_az_text_media_vimeo.js
@@ -24,52 +24,55 @@
         var tag = document.createElement("script");
         tag.src = "https://player.vimeo.com/api/player.js";
         document.head.appendChild(tag);
-        $.each(bgVideoParagraphs, function (index) {
-          var thisContainer = bgVideoParagraphs[index];
-          var parentParagraph = thisContainer.parentNode;
-          var vimeoId = thisContainer.dataset.vimeoId;
-          bgVideos[vimeoId] = $.extend({}, defaults, thisContainer);
-          var options = bgVideos[vimeoId];
-          var videoPlayer = thisContainer.getElementsByClassName("az-video-player")[0];
-          var VimeoPlayer = window.Vimeo.Player;
-          thisContainer.player = new VimeoPlayer(videoPlayer, {
-            id: vimeoId,
-            width: options.width,
-            height: Math.ceil(options.width / options.ratio),
-            autoplay: true,
-            muted: options.mute,
-            loop: options.repeat
+        tag.onload = function () {
+          $.each(bgVideoParagraphs, function (index) {
+            var thisContainer = bgVideoParagraphs[index];
+            var parentParagraph = thisContainer.parentNode;
+            var vimeoId = thisContainer.dataset.vimeoId;
+            bgVideos[vimeoId] = $.extend({}, defaults, thisContainer);
+            var options = bgVideos[vimeoId];
+            var videoPlayer = thisContainer.getElementsByClassName("az-video-player")[0];
+            var VimeoPlayer = window.Vimeo;
+            thisContainer.player = new VimeoPlayer.Player(videoPlayer, {
+              id: vimeoId,
+              width: options.width,
+              height: Math.ceil(options.width / options.ratio),
+              autoplay: true,
+              muted: options.mute,
+              loop: options.repeat
+            });
+            thisContainer.player.on("play", function () {
+              parentParagraph.classList.add("az-video-playing");
+              parentParagraph.classList.remove("az-video-paused");
+            });
+            thisContainer.player.on("pause", function () {
+              parentParagraph.classList.add("az-video-paused");
+              parentParagraph.classList.remove("az-video-playing");
+            });
+            thisContainer.player.on("ended", function () {
+              if (options.repeat) {
+                thisContainer.player.setCurrentTime(0).then(function () {
+                  thisContainer.player.play();
+                });
+              }
+            });
+            var playButton = bgVideoParagraphs[index].getElementsByClassName("az-video-play")[0];
+            playButton.addEventListener("click", function (event) {
+              event.preventDefault();
+              bgVideoParagraphs[index].player.play();
+            });
+            var pauseButton = bgVideoParagraphs[index].getElementsByClassName("az-video-pause")[0];
+            pauseButton.addEventListener("click", function (event) {
+              event.preventDefault();
+              bgVideoParagraphs[index].player.pause();
+            });
           });
-          thisContainer.player.on("play", function () {
-            parentParagraph.classList.add("az-video-playing");
-            parentParagraph.classList.remove("az-video-paused");
-          });
-          thisContainer.player.on("pause", function () {
-            parentParagraph.classList.add("az-video-paused");
-            parentParagraph.classList.remove("az-video-playing");
-          });
-          thisContainer.player.on("ended", function () {
-            if (options.repeat) {
-              thisContainer.player.setCurrentTime(0).then(function () {
-                thisContainer.player.play();
-              });
-            }
-          });
-          var playButton = bgVideoParagraphs[index].getElementsByClassName("az-video-play")[0];
-          playButton.addEventListener("click", function (event) {
-            event.preventDefault();
-            bgVideoParagraphs[index].player.play();
-          });
-          var pauseButton = bgVideoParagraphs[index].getElementsByClassName("az-video-pause")[0];
-          pauseButton.addEventListener("click", function (event) {
-            event.preventDefault();
-            bgVideoParagraphs[index].player.pause();
-          });
-        });
+        };
         var resize = function resize() {
           $.each(bgVideoParagraphs, function (index) {
             var thisContainer = bgVideoParagraphs[index];
             var videoPlayer = thisContainer.getElementsByClassName("az-video-player")[0];
+            var vimeoId = thisContainer.dataset.vimeoId;
             var ratio = bgVideos[vimeoId].ratio;
             var width = thisContainer.offsetWidth;
             var height = Math.ceil(width / ratio);
