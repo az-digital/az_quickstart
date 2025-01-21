581c4affc6628430177011870f0cbed5bce95382
Bao Nguyen <88011047+BaoNguyen09@users.noreply.github.com>
2025-01-21 15:12:07 -0700

    Update modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php
    Co-authored-by: Brian Berndt <74572157+bberndt-uaz@users.noreply.github.com>

--- modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php
+++ modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php
@@ -125,7 +125,6 @@ class AZCardDefaultFormatter extends FormatterBase implements ContainerFactoryPl
       // Link.
       $link_render_array = [];
       $link_url = '';
-      $current_path = \Drupal::service('path.current')->getPath();
       if ($item->link_title || $item->link_uri) {
         if (str_starts_with($item->link_uri ?? '', '/' . PublicStream::basePath())) {
           // Link to public file: use fromUri() to get the URL.
