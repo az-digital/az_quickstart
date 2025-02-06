df03ef1d57060cb93239a2ac0a79d982daba31e0
Chris Green <chrisgreen@arizona.edu>
2025-02-06 13:17:16 -0700

    Update test

--- modules/custom/az_demo/tests/src/Functional/AZDemoContentTest.php
+++ modules/custom/az_demo/tests/src/Functional/AZDemoContentTest.php
@@ -87,14 +87,22 @@ class AZDemoContentTest extends QuickstartFunctionalTestBase {
     $assert = $this->assertSession();
     // Assert the page loads successfully.
     $assert->statusCodeEquals(200);
-    // If field_az_link has a value, assert that the value is used as the link.
+    // If field_az_publication_link has a value, assert that the value is used as the link.
     $assert->elementExists('xpath', "//a[@href='https://example.com/ai-ethics-book' and text()='Ethics in Artificial Intelligence']");
-
+    $assert->elementNotExists('xpath', "//a[@href='/publication/ethics-artificial-intelligence' and span[text()='Ethics in Artificial Intelligence']]");
     // Ensure specific text elements are NOT links.
-    // If there is no link, cover, abstract, or page content, ensure that
-    // the title is NOT a link.
+    // If there is no value in:
+    // - field_az_publication_link
+    // - field_az_publication_image
+    // - field_az_publication_abstract
+    // ensure that the title is NOT a link, and that there is
+    // no link to the full node.
     $assert->linkNotExists('Statistical Learning with Applications');
-    // If the author is not referenced to a person, ensure that the author
+    $assert->elementNotExists('xpath', "//a[@href='/publication/statistical-learning-applications' and span[text()='Statistical Learning with Applications']]");
+    // If the author IS referenced to a person, ensure that the author
+    // IS a link.
+    $assert->linkExists('An\'ersen, José, M.A., Ph.D.');
+    // If the author is NOT referenced to a person, ensure that the author
     // is NOT a link.
     $assert->linkNotExists('van Gogh, Vincent');
     $assert->linkNotExists('Christian Andersen, Hans');
