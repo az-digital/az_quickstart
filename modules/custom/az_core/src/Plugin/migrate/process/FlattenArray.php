6c36f1008311da03faee496e1f30ec1b15bae160
Chris Green <chrisgreen@arizona.edu>
2025-02-07 22:08:56 -0700

    Close #4039 tag imported events with enterprise attributes.

---
+++ modules/custom/az_core/src/Plugin/migrate/process/FlattenArray.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\az_core\Plugin\migrate\process;
+
+use Drupal\migrate\Attribute\MigrateProcess;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Flattens a multi-dimensional array.
+ */
+#[MigrateProcess('flatten_array')]
+class FlattenArray extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($input, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (!is_array($input)) {
+      return $input;
+    }
+
+    $result = array_keys($input); // Include all first-level keys.
+
+    foreach ($input as $value) {
+      if (is_array($value) && isset($value[0]) && is_string($value[0])) {
+        // Flatten comma-separated values found in the first element of each array.
+        $result = array_merge($result, array_map('trim', explode(',', $value[0])));
+      }
+    }
+
+    return array_values(array_unique($result));
+  }
+
+}
