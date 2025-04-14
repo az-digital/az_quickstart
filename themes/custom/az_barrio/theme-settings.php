8b6aefb18646c07b38afe87a6155edf75b8d32eb
Chris Green <chrisgreen@arizona.edu>
2025-04-14 06:35:46 -0700

    Close issue/1251 separate CSS from JS in Arizona Bootstrap.

--- themes/custom/az_barrio/theme-settings.php
+++ themes/custom/az_barrio/theme-settings.php
@@ -249,15 +249,25 @@ function az_barrio_form_system_theme_settings_alter(&$form, FormStateInterface $
       ],
     ],
   ];
-  $form['azbs_settings']['settings']['az_bootstrap_cdn']['az_bootstrap_cdn_version'] = [
+  $form['azbs_settings']['settings']['az_bootstrap_cdn']['az_bootstrap_css_cdn_version'] = [
     '#type' => 'radios',
-    '#title' => t('AZ Bootstrap CDN version'),
+    '#title' => t('AZ Bootstrap CSS CDN version'),
     '#options' => [
-      'stable' => t('Stable version: This option has undergone the most testing within the az_barrio theme. Currently: %stableversion (Recommended).', ['%stableversion' => AZ_BOOTSTRAP_STABLE_VERSION]),
-      'latest-2.x' => t('Latest tagged version. The most recently tagged stable release of AZ Bootstrap. While this has not been explicitly tested on this version of az_barrio, it’s probably OK to use on production sites. Please report bugs to the AZ Digital team.'),
-      '2.x' => t('Latest dev version. This is the tip of the 2.x branch of AZ Bootstrap. Please do not use on production unless you are following the AZ Bootstrap project closely. Please report bugs to the AZ Digital team.'),
+      'stable' => t('Stable version (recommended): %version', ['%version' => AZ_BOOTSTRAP_STABLE_VERSION]),
+      'latest-2.x' => t('Latest tagged version (latest 2.x release)'),
+      '2.x' => t('Main branch (bleeding edge)'),
     ],
-    '#default_value' => theme_get_setting('az_bootstrap_cdn_version'),
+    '#default_value' => theme_get_setting('az_bootstrap_css_cdn_version'),
+  ];
+  $form['azbs_settings']['settings']['az_bootstrap_cdn']['az_bootstrap_js_cdn_version'] = [
+    '#type' => 'radios',
+    '#title' => t('AZ Bootstrap JS CDN version'),
+    '#options' => [
+      'stable' => t('Stable version (recommended): %version', ['%version' => AZ_BOOTSTRAP_STABLE_VERSION]),
+      'latest-2.x' => t('Latest tagged version (latest 2.x release)'),
+      '2.x' => t('Main branch (bleeding edge)'),
+    ],
+    '#default_value' => theme_get_setting('az_bootstrap_js_cdn_version'),
   ];
   $form['azbs_settings']['settings']['az_bootstrap_minified'] = [
     '#type'          => 'checkbox',
