eaa6f245895aeda6a9bc8e61a3ee284834eedfc7
David Celaya-Gonzalez <2702851+djcelaya@users.noreply.github.com>
2025-04-15 14:40:51 -0700

    js removes required form inputs

--- modules/custom/az_media/az_media_trellis/js/az-media-trellis.js
+++ modules/custom/az_media/az_media_trellis/js/az-media-trellis.js
@@ -3,6 +3,7 @@
     attach: function (context, settings) {
       // Access the variable passed from PHP.
       const tfaVal = drupalSettings.azMediaTrellis.tfa_4;
+      const editing = drupalSettings.azMediaTrellis.editing;
 
       // Log the variable to verify.
       console.log('tfaVal Variable:', tfaVal);
@@ -10,6 +11,25 @@
       // Use the variable as needed in your JavaScript logic.
       const formContainer = document.querySelector('#fa-form');
       const observer = new MutationObserver((mutationsList, observer) => {
+
+        console.log('editing = ' + editing);
+        if (editing) {
+          // Find all input fields inside #fa-form.
+          const inputFields = formContainer.querySelectorAll('input');
+          console.log(inputFields);
+          inputFields.forEach((input) => {
+            // Remove aria-required attribute if it exists.
+            if (input.hasAttribute('aria-required')) {
+              input.removeAttribute('aria-required');
+            }
+  
+            // Remove the "Required" class if it exists.
+            if (input.classList.contains('required')) {
+              input.classList.remove('required');
+            }
+          });
+        }
+
         const inputField = formContainer.querySelector('input[name="tfa_4"]');
         if (inputField) {
           inputField.value = tfaVal; // Use the variable here.
@@ -17,7 +37,10 @@
           observer.disconnect();
         }
       });
-      observer.observe(formContainer, { childList: true, subtree: true });
+
+
+
+      observer.observe(formContainer, { childList: true, attributes: true, subtree: true });
     }
   };
 })(Drupal, drupalSettings);
