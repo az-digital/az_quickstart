280bad9c3245118af4be9cee09b7773b3353e147
Michael Beiley <mrbeiley@email.arizona.edu>
2025-06-05 08:41:15 -0700

    Set a single border color for all cards.

--- modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php
+++ modules/custom/az_card/src/Plugin/Field/FieldFormatter/AZCardDefaultFormatter.php
@@ -222,6 +222,10 @@ class AZCardDefaultFormatter extends FormatterBase implements ContainerFactoryPl
             $title_display = $card_defaults['card_title_display'];
           }
         }
+
+        if (isset($card_defaults['card_deck_border_color'])){
+          $card_classes .= ' ' . $card_defaults['card_deck_border_color'];
+        }
       }
 
       // Handle class keys that contained multiple classes.
