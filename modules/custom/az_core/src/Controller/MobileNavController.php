3517ddf295f7e9e9ed843e87c7dba145735ef3a7
Brian Berndt <bberndt@email.arizona.edu>
2025-07-21 09:08:06 -0700

    Add alternate mobile nav block

---
+++ modules/custom/az_core/src/Controller/MobileNavController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\az_core\Controller;
+
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Block\BlockManager;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines a controller to update a mobile nav block.
+ *
+ * @internal
+ *   Controller classes are internal.
+ */
+class MobileNavController implements ContainerInjectionInterface {
+
+  /**
+   * The block manager.
+   *
+   * @var \Drupal\Core\Block\BlockManager
+   */
+  protected $blockManager;
+
+  /**
+   * MobileNavController constructor.
+   *
+   * @param \Drupal\Core\Block\BlockManager $block_manager
+   *   The block manager.
+   */
+  public function __construct(BlockManager $block_manager) {
+    $this->blockManager = $block_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('plugin.manager.block')
+    );
+  }
+
+  /**
+   * Callback for the Quickstart mobile nav block.
+   *
+   * @param string $menu_root
+   *   The menu link ID for the root of the nav menu.
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *   An AjaxResponse object.
+   */
+  public function mobileNavCallback($menu_root = '') {
+    $mobile_nav_block = $this->blockManager->createInstance('mobile_nav_block_new', ['menu_root' => $menu_root]);
+    $mobile_nav_block_build = $mobile_nav_block->build();
+
+    $response = new AjaxResponse();
+    $response->addCommand(new ReplaceCommand('#az_mobile_nav_menu_new', $mobile_nav_block_build));
+    return $response;
+  }
+
+}
