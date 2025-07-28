6cc0efb1c4a5a4704505e4df91d6cbb1eff1775c
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2025-07-28 15:18:39 -0700

    🤦‍♂️

--- modules/custom/az_core/az_enterprise_attributes_import/src/Form/AZEnterpriseAttributesImportForm.php
+++ modules/custom/az_core/az_enterprise_attributes_import/src/Form/AZEnterpriseAttributesImportForm.php
@@ -52,7 +52,7 @@ class AZEnterpriseAttributesImportForm extends ConfigFormBase {
     $instance = parent::create($container);
     $instance->pluginManagerMigration = $container->get('plugin.manager.migration');
     $instance->keyValue = $container->get('keyvalue');
-    $instance->dateTime = $container->get('datetime.time');
+    $instance->time = $container->get('datetime.time');
     $instance->translation = $container->get('string_translation');
     return $instance;
   }
