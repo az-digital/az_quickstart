b5651b9937eef74d0e4205374d5f85bc3ec06900
Brian Berndt <bberndt@email.arizona.edu>
2025-10-30 15:35:32 -0700

    Adds .es6.js files, update no-follow .js

--- modules/custom/az_ranking/js/az-ranking-no-follow.js
+++ modules/custom/az_ranking/js/az-ranking-no-follow.js
@@ -4,22 +4,16 @@
 * https://www.drupal.org/node/2815083
 * @preserve
 **/
-function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
-function _iterableToArray(r) { if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r); }
-function _arrayWithoutHoles(r) { if (Array.isArray(r)) return _arrayLikeToArray(r); }
-function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
-(function (Drupal) {
+(Drupal => {
   Drupal.behaviors.azRankingNoFollow = {
-    attach: function attach(context) {
+    attach(context) {
       function noFollow(event) {
         event.preventDefault();
       }
-      var rankings = context.querySelectorAll('.az-ranking-no-follow');
-      _toConsumableArray(rankings).forEach(function (ranking) {
+      const rankings = context.querySelectorAll('.az-ranking-no-follow');
+      [...rankings].forEach(ranking => {
         ranking.addEventListener('click', noFollow);
       });
     }
   };
-})(this.Drupal);
+})(Drupal);
