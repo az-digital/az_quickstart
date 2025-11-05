b2ee5d92944ac2b2981657e701a006246ab9439e
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2025-11-05 16:06:46 -0700

    Restore media--type-- classes added by AZTextWithMediaParagraphBehavior

--- modules/custom/az_paragraphs/src/Plugin/paragraphs/Behavior/AZTextWithMediaParagraphBehavior.php
+++ modules/custom/az_paragraphs/src/Plugin/paragraphs/Behavior/AZTextWithMediaParagraphBehavior.php
@@ -209,7 +209,7 @@ class AZTextWithMediaParagraphBehavior extends AZDefaultParagraphsBehavior {
     $variables['attributes']['class'][] = 'paragraph';
     $variables['attributes']['class'][] = 'position-relative';
     $variables['attributes']['class'][] = HTML::getClass('paragraph--type--' . $paragraph->bundle());
-    if (!empty($variables['content']['field_az_media']['#media_type'])) {
+    if (!empty($variables['content']['field_az_media'][0]['#media_type'])) {
       $variables['attributes']['class'][] = 'media--type--' . HTML::getClass($variables['content']['field_az_media']['#media_type']);
     }
     $variables['attributes']['class'][] = HTML::getClass('paragraph--view-mode--' . $variables['view_mode']);
