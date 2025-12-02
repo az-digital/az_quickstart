c016a1d3bac27848770b0a01728ada31cf68a6de
Brian Berndt <bberndt@email.arizona.edu>
2025-12-01 17:12:24 -0700

    ESLint fixes

--- themes/custom/az_barrio/js/az-barrio-sidebar-collapsible-menu.js
+++ themes/custom/az_barrio/js/az-barrio-sidebar-collapsible-menu.js
@@ -2,14 +2,17 @@
   Drupal.behaviors.azBarrioSidebarCollapsibleMenu = {
     attach: (context) => {
       function addParentItemStyling() {
-        once('addParentItemStyling', '.az-sidebar-collapsible-menu').forEach(() => {
-          const menuParent = document.querySelector(
-            '.az-sidebar-collapsible-parent > a',
-          );
-          if (menuParent && menuParent.classList) {
-            menuParent.classList.add('nav-link');
-          }
-        }, context);
+        once('addParentItemStyling', '.az-sidebar-collapsible-menu').forEach(
+          () => {
+            const menuParent = document.querySelector(
+              '.az-sidebar-collapsible-parent > a',
+            );
+            if (menuParent && menuParent.classList) {
+              menuParent.classList.add('nav-link');
+            }
+          },
+          context,
+        );
       }
 
       once('addMenuParentStyling', '#az-sidebar-collapsible').forEach(
