dfcba9bb021411115af04243875a9e0dc8288e58
Z Saenz <zsaenz@arizona.edu>
2025-12-10 10:49:41 -0700

    eslint

--- themes/custom/az_barrio/js/az-back-to-top.js
+++ themes/custom/az_barrio/js/az-back-to-top.js
@@ -5,14 +5,17 @@
  */
 
 (function (Drupal, window, document) {
-  'use strict';
   Drupal.behaviors.uaBackToTop = {
-    attach: function (context, settings) {
+    attach(context, settings) {
       // only run this script if the document height is 4 times the height
       // of the browser window the page is being viewed through.
-      if(document.body.clientHeight / document.querySelector('html').clientHeight >= 4) {
+      if (
+        document.body.clientHeight /
+          document.querySelector('html').clientHeight >=
+        4
+      ) {
         const backToTop = document.getElementById('az-back-to-top');
-        
+
         // Smoothly scroll to the top of the page if the arrow is clicked.
         backToTop.addEventListener('click', function () {
           window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -27,6 +30,6 @@
           }
         });
       }
-    }
+    },
   };
 })(Drupal, window, document);
