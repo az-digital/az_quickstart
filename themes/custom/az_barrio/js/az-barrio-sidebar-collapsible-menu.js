f5bd2c1896da964fdd42c84bc5a911ae02d1c2ab
Brian Berndt <bberndt@email.arizona.edu>
2026-02-10 09:45:10 -0700

    Only one submenu can be open at a time

--- themes/custom/az_barrio/js/az-barrio-sidebar-collapsible-menu.js
+++ themes/custom/az_barrio/js/az-barrio-sidebar-collapsible-menu.js
@@ -1,13 +1,51 @@
 ((Drupal, once) => {
   Drupal.behaviors.azBarrioSidebarCollapsibleMenu = {
     attach: (context) => {
+      const azSidebarCollapsibleMenu = document.querySelector(
+        '.az-sidebar-collapsible-menu',
+      );
+      function closeOtherMenuItems(shownMenuItemContent) {
+        const collapseElementsNotInActiveTrail =
+          azSidebarCollapsibleMenu.querySelectorAll(
+            `.collapse:not(:has([id='${shownMenuItemContent.id}']))`,
+          );
+        const collapseElementsToClose = [];
+        [...collapseElementsNotInActiveTrail].forEach((el) => {
+          if (el !== shownMenuItemContent && el.classList.contains('show')) {
+            collapseElementsToClose.push(el);
+          }
+        });
+        if (collapseElementsToClose.length !== 0) {
+          [...collapseElementsToClose].forEach((el) => {
+            azSidebarCollapsibleMenu
+              .querySelector(`[href='#${el.id}']`)
+              .click();
+          });
+        }
+      }
+      function handleCollapseEvent(event) {
+        if (event.target.classList.contains('az-sidebar-collapse')) {
+          closeOtherMenuItems(event.target);
+        }
+      }
+
       once('addParentStyling', '.az-sidebar-collapsible-menu').forEach(() => {
-        const menuParent = document.querySelector(
+        const menuParent = azSidebarCollapsibleMenu.querySelector(
           '.az-sidebar-collapsible-parent > a',
         );
         if (menuParent && menuParent.classList) {
           menuParent.classList.add('nav-link');
         }
+      }, context);
+
+      once(
+        'addMenuItemCollapseHandling',
+        '.az-sidebar-collapsible-menu',
+      ).forEach(() => {
+        azSidebarCollapsibleMenu.addEventListener(
+          'show.bs.collapse',
+          handleCollapseEvent,
+        );
       }, context);
     },
   };
