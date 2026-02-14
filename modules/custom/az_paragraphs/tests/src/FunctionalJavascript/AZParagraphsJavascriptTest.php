0acc6fcd6d8a66e3ec8aba09bfccc4d808ce245a
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2026-02-13 22:51:19 -0700

    Clean up annotations order

--- modules/custom/az_paragraphs/tests/src/FunctionalJavascript/AZParagraphsJavascriptTest.php
+++ modules/custom/az_paragraphs/tests/src/FunctionalJavascript/AZParagraphsJavascriptTest.php
@@ -4,15 +4,15 @@ namespace Drupal\Tests\az_paragraphs\FunctionalJavascript;
 
 use Drupal\Tests\az_core\FunctionalJavascript\QuickstartFunctionalJavascriptTestBase;
 use PHPUnit\Framework\Attributes\Group;
-use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use PHPUnit\Framework\Attributes\IgnoreDeprecations;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
 /**
  * Run tests of paragraph bundles.
  */
 #[Group('az_paragraphs_js')]
-#[RunTestsInSeparateProcesses]
 #[IgnoreDeprecations]
+#[RunTestsInSeparateProcesses]
 class AZParagraphsJavascriptTest extends QuickstartFunctionalJavascriptTestBase {
 
   /**
