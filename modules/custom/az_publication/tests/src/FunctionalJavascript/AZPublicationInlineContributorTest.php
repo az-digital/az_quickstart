9577d59bea77989b0dc462aed4acd27344d052d2
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2026-02-13 22:53:14 -0700

    Reorder use statements in AZPublicationInlineContributorTest

--- modules/custom/az_publication/tests/src/FunctionalJavascript/AZPublicationInlineContributorTest.php
+++ modules/custom/az_publication/tests/src/FunctionalJavascript/AZPublicationInlineContributorTest.php
@@ -4,9 +4,9 @@ namespace Drupal\Tests\az_publication\FunctionalJavascript;
 
 use Drupal\Tests\az_core\FunctionalJavascript\QuickstartFunctionalJavascriptTestBase;
 use PHPUnit\Framework\Attributes\Group;
-use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use PHPUnit\Framework\Attributes\IgnoreDeprecations;
 use PHPUnit\Framework\Attributes\IgnorePhpunitDeprecations;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
 /**
  * Run tests of publication contributor role functionality.
