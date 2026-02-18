b214c37140c91d2dfbf4b674f6cf112fe017465a
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2026-02-18 09:54:06 -0700

    Apply suggestions from code review

--- modules/custom/az_paragraphs/tests/src/FunctionalJavascript/AZParagraphsJavascriptTest.php
+++ modules/custom/az_paragraphs/tests/src/FunctionalJavascript/AZParagraphsJavascriptTest.php
@@ -4,14 +4,12 @@ namespace Drupal\Tests\az_paragraphs\FunctionalJavascript;
 
 use Drupal\Tests\az_core\FunctionalJavascript\QuickstartFunctionalJavascriptTestBase;
 use PHPUnit\Framework\Attributes\Group;
-use PHPUnit\Framework\Attributes\IgnoreDeprecations;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
 /**
  * Run tests of paragraph bundles.
  */
 #[Group('az_paragraphs_js')]
-#[IgnoreDeprecations]
 #[RunTestsInSeparateProcesses]
 class AZParagraphsJavascriptTest extends QuickstartFunctionalJavascriptTestBase {
 
