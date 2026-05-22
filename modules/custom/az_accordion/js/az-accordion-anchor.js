4d4a395b83fcc626216e128de3c5a41424664e66
Z Saenz <zsaenz@arizona.edu>
2026-05-22 14:42:56 -0700

    eslint fixes

--- modules/custom/az_accordion/js/az-accordion-anchor.js
+++ modules/custom/az_accordion/js/az-accordion-anchor.js
@@ -5,45 +5,51 @@
  * anchor link scrolling and expand content.
  */
 
-/* 
+/**
+ * Expands accordion and scrolls to anchor link header with vertical offset.
+ */
+function scrollToAccordion() {
+  const scrollOffset = 75; // How many Pixels above the scroll target (prevents anchor scroll being on very top of screen)
+  const anchortag = window.location.hash.substring(1); // Get anchor link hash without the #
+
+  if (!anchortag) {
+    // No anchor link found
+    return;
+  }
+
+  // Get the accordion
+  const parentAccordion = document.querySelector(
+    `[data-bs-target="#${anchortag}"]`,
+  );
+  if (parentAccordion !== null) {
+    // Un-collapse parent accordion if collapsed
+    if (parentAccordion.classList.contains('collapsed')) {
+      parentAccordion.classList.remove('collapsed');
+      parentAccordion.setAttribute('aria-expanded', 'true');
+      const accordionBodyElem = document.getElementById(anchortag);
+      accordionBodyElem.classList.add('show');
+    }
+
+    // Scroll to element with offset
+    const elementPosition =
+      parentAccordion.getBoundingClientRect().top + window.pageYOffset;
+    window.scrollTo({
+      top: elementPosition - scrollOffset,
+      behavior: 'smooth',
+    });
+  }
+}
+
+/*
  * Scroll when loading document and when changing the hash URL
  */
 document.addEventListener('DOMContentLoaded', scrollToAccordion);
 window.addEventListener('hashchange', scrollToAccordion);
 
 /**
- * Expands accordion and scrolls to anchor link header with vertical offset.
- */
-function scrollToAccordion() {
-  var scroll_offset = 75; // How many Pixels above the scroll target (prevents anchor scroll being on very top of screen)
-  var anchortag = window.location.hash.substring(1); // Get anchor link hash without the #
-  
-  if (!anchortag) { // No anchor link found
-    return;
-  }
-
-  // Get the accordion
-  var parent_accordion = document.querySelector('[data-bs-target="#' + anchortag + '"]'); 
-  if (parent_accordion !== null) {
-    // Un-collapse parent accordion if collapsed
-    if (parent_accordion.classList.contains("collapsed")) {
-      parent_accordion.classList.remove('collapsed');
-      parent_accordion.setAttribute('aria-expanded', 'true');
-      var accordion_body_elem = document.getElementById(anchortag);
-      accordion_body_elem.classList.add('show');
-    }
-    
-    // Scroll to element with offset
-    var elementPosition = parent_accordion.getBoundingClientRect().top + window.pageYOffset;
-    window.scrollTo({
-      top: elementPosition - scroll_offset,
-      behavior: 'smooth'
-    });
-  }
-}
-
-/**
- * Copies anchor link when clicked
+ * Copies anchor link when clicked.
+ * @param accordionId - the id of the accordion from Twig.
+ * @param event - the onclick event
  */
 function copyAnchor(accordionId, event) {
   // Prevents anchor link from activating on click
@@ -56,15 +62,16 @@ function copyAnchor(accordionId, event) {
   const baseUrl = window.location.href.split('#')[0];
 
   // Construct the URL with the accordion ID anchor
-  const urlToCopy = baseUrl + '#' + accordionId;
+  const urlToCopy = `${baseUrl}#${accordionId}`;
 
   // Copy to clipboard using the Clipboard API
-  navigator.clipboard.writeText(urlToCopy)
+  navigator.clipboard
+    .writeText(urlToCopy)
     .then(() => {
-      //TODO: Visual Feedback, popup modal or temp icon animation
+      // TODO: Visual Feedback, popup modal or temp icon animation
       console.log('URL copied to clipboard:', urlToCopy);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error('Failed to copy URL to clipboard:', err);
     });
 }
