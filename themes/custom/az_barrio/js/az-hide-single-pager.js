1509e6d587195945772f89aa6e6ffe1b958a5c84
Z Saenz <zsaenz@arizona.edu>
2026-06-26 08:05:16 -0700

    cbf and lint

--- themes/custom/az_barrio/js/az-hide-single-pager.js
+++ themes/custom/az_barrio/js/az-hide-single-pager.js
@@ -1,11 +1,11 @@
 (function (Drupal, once) {
-    Drupal.behaviors.hidePageSummary = {
-        attach: function (context) {
-            // Find the pager summary and hide it
-            const elements = context.querySelectorAll('#az-page-summary');
-            elements.forEach(element => {
-                element.classList.add('d-none');
-            });
-        },
-    };
+  Drupal.behaviors.hidePageSummary = {
+    attach(context) {
+      // Find the pager summary and hide it
+      const elements = once('hide-summary-key', '#az-page-summary', context);
+      elements.forEach((element) => {
+        element.classList.add('d-none');
+      });
+    },
+  };
 })(Drupal, once);
