43685ae3bc9c248d804482c50ddb28d24916b8ba
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2026-09-09 09:54:44 -0700

    Prettier fixes.

--- themes/custom/az_barrio/js/az-tooltips.js
+++ themes/custom/az_barrio/js/az-tooltips.js
@@ -8,15 +8,13 @@
 ((Drupal, once) => {
   Drupal.behaviors.azAccordionAnchors = {
     attach: (context) => {
-      once(
-        'az-tooltips',
-        '[data-bs-toggle="tooltip"]',
-        context,
-      ).forEach((trigger) => {
-        arizonaBootstrap.Tooltip.getOrCreateInstance(trigger, {
-          container: 'body',
-        });
-      });
+      once('az-tooltips', '[data-bs-toggle="tooltip"]', context).forEach(
+        (trigger) => {
+          arizonaBootstrap.Tooltip.getOrCreateInstance(trigger, {
+            container: 'body',
+          });
+        },
+      );
     },
   };
 })(Drupal, once);
