e234469fd1eacf8d125bbf92e748bf536dd3e91e
Z Saenz <zsaenz@arizona.edu>
2026-09-09 14:19:17 -0700

    refactor setting to theme

--- themes/custom/az_barrio/theme-settings.php
+++ themes/custom/az_barrio/theme-settings.php
@@ -298,6 +298,12 @@ function az_barrio_form_system_theme_settings_alter(&$form, FormStateInterface $
     '#title' => t('Use the AZ Bootstrap sticky footer template.'),
     '#default_value' => \Drupal::service('Drupal\Core\Extension\ThemeSettingsProvider')->getSetting('sticky_footer'),
   ];
+  $form['azbs_settings']['settings']['az_bootstrap_style']['heading_font_styles'] = [
+    '#title' => t('Use Serif headings'),
+    '#type' => 'checkbox',
+    '#description' => t("Use the Garamond serif font for headings H1, H2, and H3."),
+    '#default_value' => \Drupal::service('Drupal\Core\Extension\ThemeSettingsProvider')->getSetting('heading_font_styles'),
+  ];
   // Responsive Header Grid.
   $form['layout']['header_grid'] = [
     '#type' => 'details',
